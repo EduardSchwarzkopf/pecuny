710d1ccbdfd8d5da66231c6710847ae1c2a14e2b
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-23 11:15:10 +0100

    main file added

---
+++ app/main.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI
+from . import models
+from .database import engine
+
+# from .routers import users, posts, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
+
+# create all tables that are not created yet
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI()
+
+# Allowed Domains to talk to this api
+origins = ["http://localhost"]
+
+# CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Routes
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, new stuff"}
