9252c505278748d1a2c6de26b49a8c92e392aa4e
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-23 07:45:33 +0100

    added schema file

---
+++ app/schemas.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+from pydantic.types import conint
+
+
+class Base(BaseModel):
+    class Config:
+        orm_mode = True
+
+
+class UserBase(Base):
+    email: EmailStr
+
+
+class UserCreate(UserBase):
+    password: str
+
+
+class UserData(UserBase):
+    id: int
+
+
+class UserLogin(UserCreate):
+    pass
