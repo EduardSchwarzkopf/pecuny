91c2c8259b987ff3be29b3a042e76381f226e4ae
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-23 13:13:57 +0100

    updated allowed methods

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE"],
     allow_headers=["*"],
 )
 
