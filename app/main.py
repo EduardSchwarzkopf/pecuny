1e9eb987d7300a059e5dd7b3aaf8ea11f9bfbd9c
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-26 07:18:19 +0100

    global db session added

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
+from fastapi_sqlalchemy import DBSessionMiddleware
 from . import models
-from .database import engine
+from .database import engine, SQLALCHEMY_DATABASE_URL
+from .routers import users
 
 # from .routers import users, posts, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
@@ -22,8 +24,11 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Database
+app.add_middleware(DBSessionMiddleware, db_url=SQLALCHEMY_DATABASE_URL)
+
 # Routes
-# app.include_router(users.router)
+app.include_router(users.router)
 
 
 @app.get("/")
