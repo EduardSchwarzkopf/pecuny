cb60223e929ae4c58997925f0feeca7eb8cc5914
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-27 07:05:31 +0100

    added testing

---
+++ tests/conftest.py
@@ -0,0 +1,47 @@
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from fastapi_sqlalchemy import DBSessionMiddleware
+from app.main import app
+from app.config import settings
+from app.database import Base
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_passwort}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+app.add_middleware(DBSessionMiddleware, db_url=SQLALCHEMY_DATABASE_URL)
+
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture()
+def client(session):
+    yield TestClient(app)
+
+
+@pytest.fixture()
+def test_user(client):
+    user_data = {"email": "hello123@pytest.de", "password": "password123"}
+
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user["password"] = user_data["password"]
+    return new_user
+
+
+# @pytest.fixture()
+# def token(test_user):
+#     return create_access_token({"user_id": test_user["id"]})
+
+
+# @pytest.fixture()
+# def authorized_client(client, token):
+#     client.headers = {**client.headers, "Authorization": "Bearer " + token}
+
+#     return client
---
+++ tests/test_users.py
@@ -0,0 +1,57 @@
+import pytest
+from jose import jwt
+
+from app import schemas
+from app.config import settings
+
+#
+# use with: pytest --disable-warnings -v -x
+#
+
+
+def test_create_user(client):
+    res = client.post(
+        "/users/",
+        json={
+            "username": "my_username",
+            "email": "hello123@pytest.de",
+            "password": "password123",
+        },
+    )
+    new_user = schemas.UserData(**res.json())
+
+    assert new_user.email == "hello123@pytest.de"
+    assert res.status_code == 201
+
+
+# def test_login_user(client, test_user):
+#     res = client.post(
+#         "/login",
+#         data={"username": test_user["email"], "password": test_user["password"]},
+#     )
+
+#     login_res = schemas.Token(**res.json())
+#     payload = jwt.decode(
+#         login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+#     )
+#     id = payload["user_id"]
+
+#     assert id == test_user["id"]
+#     assert login_res.token_type == "bearer"
+#     assert res.status_code == 200
+
+
+# @pytest.mark.parametrize(
+#     "email, password, status_code",
+#     [
+#         ("wrongemail@gmail.com", "password123", 403),
+#         ("hello123@pytest.de", "wrongPassword", 403),
+#         ("wrongemail@gmail.com", "wrongPassword", 403),
+#         (None, "wrongPassword", 422),
+#         ("wrongemail@gmail.com", None, 422),
+#     ],
+# )
+# def test_invalid_login(client, test_user, email, password, status_code):
+#     res = client.post("/login", data={"username": email, "password": password})
+
+#     assert res.status_code == status_code
