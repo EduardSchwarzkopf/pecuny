bbd00e5ca3d69603306e5a598232fe68bd0ec23e
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-27 06:36:26 +0100

    removed local session

--- app/database.py
+++ app/database.py
@@ -6,6 +6,5 @@ from .config import settings
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_passwort}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
