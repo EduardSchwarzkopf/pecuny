a1edf0932afff18c5d3dec7da38507dc0aba0174
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 17:35:21 +0100

    added models check for refreshing

--- app/transaction_manager.py
+++ app/transaction_manager.py
@@ -1,14 +1,20 @@
 from fastapi_sqlalchemy import db
+from . import models
 
 
 def transaction(handler, *args: dict) -> dict:
     try:
         result = handler(*args)
         db.session.commit()
-        db.session.refresh(result)
+        if _is_models_object(result):
+            db.session.refresh(result)
 
     except Exception as e:
         result = {}
         db.session.rollback()
     finally:
         return result
+
+
+def _is_models_object(db_object):
+    return getattr(db_object, "__module__", None) == models.__name__
