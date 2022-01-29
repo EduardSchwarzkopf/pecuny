b9bd5cc5928ff9c9c9d2e1a0be5a8df9af7f365c
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 06:24:50 +0100

    login tests added

--- tests/test_users.py
+++ tests/test_users.py
@@ -26,34 +26,45 @@ def test_create_user(client):
     assert res.status_code == 201
 
 
-# def test_login_user(client, test_user):
-#     res = client.post(
-#         "/login",
-#         data={"username": test_user["email"], "password": test_user["password"]},
-#     )
+@pytest.mark.parametrize(
+    "username, password, status_code",
+    [
+        ("hello123@pytest.de", "password123", 200),
+        ("testuser", "password123", 200),
+        ("TESTUSER", "password123", 200),
+        ("TestUser", "password123", 200),
+        ("testUser", "password123", 200),
+    ],
+)
+def test_valid_login(client, test_user, username, password, status_code):
+    res = client.post(
+        "/login",
+        data={"username": username, "password": password},
+    )
 
-#     login_res = schemas.Token(**res.json())
-#     payload = jwt.decode(
-#         login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
-#     )
-#     id = payload["user_id"]
+    login_res = schemas.Token(**res.json())
+    payload = jwt.decode(
+        login_res.access_token, settings.secret_key, algorithms=[settings.algorithm]
+    )
+    id = payload["user_id"]
 
-#     assert id == test_user["id"]
-#     assert login_res.token_type == "bearer"
-#     assert res.status_code == 200
+    assert id == test_user["id"]
+    assert login_res.token_type == "bearer"
+    assert res.status_code == 200
 
 
-# @pytest.mark.parametrize(
-#     "email, password, status_code",
-#     [
-#         ("wrongemail@gmail.com", "password123", 403),
-#         ("hello123@pytest.de", "wrongPassword", 403),
-#         ("wrongemail@gmail.com", "wrongPassword", 403),
-#         (None, "wrongPassword", 422),
-#         ("wrongemail@gmail.com", None, 422),
-#     ],
-# )
-# def test_invalid_login(client, test_user, email, password, status_code):
-#     res = client.post("/login", data={"username": email, "password": password})
+@pytest.mark.parametrize(
+    "username, password, status_code",
+    [
+        ("wrongemail@gmail.com", "password123", 403),
+        ("hello123@pytest.de", "wrongPassword", 403),
+        ("aaaa", "wrongPassword", 403),
+        ("*39goa", "wrongPassword", 403),
+        (None, "wrongPassword", 422),
+        ("wrongemail@gmail.com", None, 422),
+    ],
+)
+def test_invalid_login(client, test_user, username, password, status_code):
+    res = client.post("/login", data={"username": username, "password": password})
 
-#     assert res.status_code == status_code
+    assert res.status_code == status_code
