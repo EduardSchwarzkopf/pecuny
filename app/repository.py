44a6e808473e24df5a9fee76b0711c51eb592357
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 11:20:54 +0100

    added typing

--- app/repository.py
+++ app/repository.py
@@ -1,42 +1,43 @@
 from sqlalchemy import or_, extract
 from . import models
 from fastapi_sqlalchemy import db
+from datetime import datetime
 
 
-def __str_to_class(classname):
+def __str_to_class(classname: str):
     return getattr(models, classname)
 
 
-def __db_action(action_name, object):
+def __db_action(action_name: str, object):
     method = getattr(db.session, action_name)
     method(object)
 
 
-def __db_query_action(filter_option, name, attribute, value):
+def __db_query_action(filter_option: str, name: str, attribute: str, value: str):
     class_ = __str_to_class(name)
     attr = getattr(class_, attribute)
     return getattr(db.session.query(class_), filter_option)(attr == value)
 
 
-def get_all(name):
+def get_all(name: str):
     class_ = __str_to_class(name)
     return db.session.query(class_).all()
 
 
-def filter(name, attribute, value):
+def filter(name: str, attribute: str, value: str):
     return __db_query_action("filter", name, attribute, value)
 
 
-def filter_by(name, attribute, value):
+def filter_by(name: str, attribute: str, value: str):
     return __db_query_action("filter_by", name, attribute, value)
 
 
-def get(name, id):
+def get(name: str, id: int):
     class_ = __str_to_class(name)
     return db.session.query(class_).get(id)
 
 
-def get_from_month(name, date, attribute, value):
+def get_from_month(name: str, date: datetime, attribute: str, value: str):
     class_ = __str_to_class(name)
     attribute = getattr(class_, attribute)
 
@@ -55,7 +56,7 @@ def get_from_month(name, date, attribute, value):
     )
 
 
-def get_scheduled_transactions_for_date(date):
+def get_scheduled_transactions_for_date(date: datetime):
     ts = models.TransactionScheduled
     return (
         db.session.query(ts)
@@ -65,16 +66,16 @@ def get_scheduled_transactions_for_date(date):
     )
 
 
-def save(object):
+def save(object: models):
     if isinstance(object, list):
         return __db_action("add_all", object)
 
     return __db_action("add", object)
 
 
-def delete(object):
+def delete(object: models):
     return __db_action("delete", object)
 
 
-def refresh(object):
+def refresh(object: models):
     return __db_action("refresh", object)
