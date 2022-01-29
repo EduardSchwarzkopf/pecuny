ca1cb1435c22fc9b3d56db990f1140d24bf5fea3
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 11:21:48 +0100

    update user logic added

--- app/services/users.py
+++ app/services/users.py
@@ -31,8 +31,20 @@ def create_user(user: schemas.UserCreate):
     return new_user
 
 
-def update_user(user: schemas.UserData):
-    pass
+def update_user(user_id: int, user: schemas.UserUpdate):
+    db_user = repo.get("User", user_id)
+
+    for key, value in user.dict().items():
+        # skip empty values or same as previous
+        if not value or value == getattr(db_user, key):
+            continue
+
+        if key == "password":
+            value = utils.hash(value)
+
+        setattr(db_user, key, value)
+
+    return db_user
 
 
 def delete_user(user_id: int):
