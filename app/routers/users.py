fdf5208d49dd38df696c07bf618631776959fd48
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 11:21:36 +0100

    update user route implemented

--- app/routers/users.py
+++ app/routers/users.py
@@ -26,11 +26,34 @@ def create_user(
 
 @router.put("/{user_id}", response_model=response_model)
 def update_user(
-    user: schemas.UserData,
+    user_id: int,
+    user_data: schemas.UserUpdate,
     current_user: models.User = Depends(oauth2.get_current_user),
 ):
-    user = tm.transaction(service.update_user, user)
-    return user
+    if current_user.id != user_id:
+        raise HTTPException(
+            status.HTTP_403_FORBIDDEN, detail="You are not allowed to update this user"
+        )
+
+    email_user = service.get_user_by_email(user_data.email)
+    if email_user and current_user != email_user:
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, detail="E-Mail address already in use"
+        )
+
+    username_user = service.get_user_by_username(user_data.username)
+    if username_user and current_user != username_user:
+        raise HTTPException(
+            status.HTTP_409_CONFLICT, detail="Username address already in use"
+        )
+
+    if not user_data.password and not user_data.password.strip():
+        raise HTTPException(
+            status.HTTP_400_BAD_REQUEST, detail="Empty Password not allowed"
+        )
+
+    updated_user = tm.transaction(service.update_user, user_id, user_data)
+    return updated_user
 
 
 @router.delete(
