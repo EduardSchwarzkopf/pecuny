01860ae28b81910ebb629ef9497f4dd8d217dd5f
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 06:26:53 +0100

    auth user service added

--- app/services/users.py
+++ app/services/users.py
@@ -1,6 +1,20 @@
 from .. import models, schemas, utils, repository as repo
 
 
+def auth_user(user_credentials):
+
+    username = user_credentials.username.lower()
+    user = repo.filter("User", "email", username).first()
+
+    if not user:
+        user = repo.filter("User", "username", username).first()
+
+    if not user or not utils.verify(user_credentials.password, user.password):
+        return None
+
+    return user
+
+
 def create_user(user: schemas.UserCreate):
 
     user.password = utils.hash(user.password)
