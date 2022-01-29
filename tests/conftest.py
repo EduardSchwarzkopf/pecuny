c31f8ddcb95d2d4c16ea50f80329b906f9516922
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-01-29 12:36:54 +0100

    authorized client added

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,7 @@ from fastapi_sqlalchemy import DBSessionMiddleware
 from app.main import app
 from app.config import settings
 from app.database import Base
+from app.oauth2 import create_access_token
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_passwort}@{settings.db_host}:{settings.db_port}/{settings.db_name}_test"
 
@@ -39,13 +40,13 @@ def test_user(client):
     return new_user
 
 
-# @pytest.fixture()
-# def token(test_user):
-#     return create_access_token({"user_id": test_user["id"]})
+@pytest.fixture()
+def token(test_user):
+    return create_access_token({"user_id": test_user["id"]})
 
 
-# @pytest.fixture()
-# def authorized_client(client, token):
-#     client.headers = {**client.headers, "Authorization": "Bearer " + token}
+@pytest.fixture()
+def authorized_client(client, token):
+    client.headers = {**client.headers, "Authorization": "Bearer " + token}
 
-#     return client
+    return client
