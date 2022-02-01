9227ef5af8f1669d8dd2b04bf41eec85316e5d84
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-02-01 06:55:14 +0100

    added transaction router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi_sqlalchemy import DBSessionMiddleware
 from . import models
 from .database import engine, SQLALCHEMY_DATABASE_URL
-from .routers import users, auth, accounts
+from .routers import users, auth, accounts, transactions
 
 # from .routers import users, posts, auth, vote
 from fastapi.middleware.cors import CORSMiddleware
@@ -31,6 +31,7 @@ app.add_middleware(DBSessionMiddleware, db_url=SQLALCHEMY_DATABASE_URL)
 app.include_router(users.router)
 app.include_router(auth.router)
 app.include_router(accounts.router)
+app.include_router(transactions.router)
 
 
 @app.get("/")
