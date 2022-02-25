d01363f4c9bdb590fab4599857f096e279744f00
48969167 <mail.eduard.schwarzkopf@gmail.com>
2022-02-25 16:24:16 +0100

    seed data refactored

--- app/events.py
+++ app/events.py
@@ -1,149 +1,297 @@
+from app import models
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy import table, column
 from sqlalchemy.orm.session import Session
 
 
-def create_categories(db_session: Session):
-    """Create transaction categories."""
-    from .models import TransactionSubcategory, TransactionCategory
+INCOME_ID = 1
+HOUSING_ID = 2
+HOUSHOLD_ID = 3
+LEISURE_ID = 4
+WORK_ID = 5
+TRANSPORT_ID = 6
+EDUCATION_ID = 7
+INSURANCE_ID = 8
+FINANCE_ID = 9
+OTHER_ID = 10
 
-    income_cats = [("Salary", True), ("Support", True), ("Other income", True)]
+
+def _get_subcategory_list():
+    income_cats = [
+        {"label": "Salary", "is_income": True, "parent_category_id": INCOME_ID},
+        {"label": "Support", "is_income": True, "parent_category_id": INCOME_ID},
+        {"label": "Other income", "is_income": True, "parent_category_id": INCOME_ID},
+    ]
 
     housing_cats = [
-        ("Rent", False),
-        ("Heating", False),
-        ("Electricity", False),
-        ("Water", False),
-        ("Renovation/Repair", False),
-        ("TV licence", False),  # de: GEZ
-        ("Phone and internet", False),
-        ("Furniture", False),
-        ("Electric appliances", False),
+        {"label": "Rent", "is_income": False, "parent_category_id": HOUSING_ID},
+        {"label": "Heating", "is_income": False, "parent_category_id": HOUSING_ID},
+        {"label": "Electricity", "is_income": False, "parent_category_id": HOUSING_ID},
+        {"label": "Water", "is_income": False, "parent_category_id": HOUSING_ID},
+        {
+            "label": "Renovation/Repair",
+            "is_income": False,
+            "parent_category_id": HOUSING_ID,
+        },
+        {
+            "label": "TV licence",
+            "is_income": False,
+            "parent_category_id": HOUSING_ID,
+        },  # de: GEZ
+        {
+            "label": "Phone and internet",
+            "is_income": False,
+            "parent_category_id": HOUSING_ID,
+        },
+        {"label": "Furniture", "is_income": False, "parent_category_id": HOUSING_ID},
+        {
+            "label": "Electric appliances",
+            "is_income": False,
+            "parent_category_id": HOUSING_ID,
+        },
     ]
 
     houshold_cats = [
-        ("Detergent", False),
-        ("Groceries", False),
-        ("Beverages", False),
-        ("Drink and tobacco", False),  # de Genussmittell
-        ("Medication", False),
-        ("Hygiene products", False),
+        {"label": "Detergent", "is_income": False, "parent_category_id": HOUSHOLD_ID},
+        {"label": "Groceries", "is_income": False, "parent_category_id": HOUSHOLD_ID},
+        {"label": "Beverages", "is_income": False, "parent_category_id": HOUSHOLD_ID},
+        {
+            "label": "Drink and tobacco",
+            "is_income": False,
+            "parent_category_id": HOUSHOLD_ID,
+        },  # de Genussmittell
+        {"label": "Medication", "is_income": False, "parent_category_id": HOUSHOLD_ID},
+        {
+            "label": "Hygiene products",
+            "is_income": False,
+            "parent_category_id": HOUSHOLD_ID,
+        },
     ]
 
     leisure_cats = [
-        ("Restaurant", False),
-        ("Hobby", False),
-        ("Gym", False),
-        ("Pets", False),
-        ("Membership contribution", False),  # de: Vereinsbeiträge
-        ("Gaming", False),
-        ("Vacation", False),
-        ("Trips", False),
-        ("Hairdresser", False),
-        ("Cosmetic", False),
-        ("Clothing", False),
-        ("Magazines and books", False),
+        {"label": "Restaurant", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Hobby", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Gym", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Pets", "is_income": False, "parent_category_id": LEISURE_ID},
+        {
+            "label": "Membership contribution",
+            "is_income": False,
+            "parent_category_id": LEISURE_ID,
+        },  # de: Vereinsbeiträge
+        {"label": "Gaming", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Vacation", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Trips", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Hairdresser", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Cosmetic", "is_income": False, "parent_category_id": LEISURE_ID},
+        {"label": "Clothing", "is_income": False, "parent_category_id": LEISURE_ID},
+        {
+            "label": "Magazines and books",
+            "is_income": False,
+            "parent_category_id": LEISURE_ID,
+        },
     ]
 
     work_cats = [
-        ("Furniture", False),
-        ("Work materials", False),  # de: Arbeitsutensilien
-        ("Mailings", False),
-        ("Workwear", False),
-        ("Travel expenses", False),
+        {"label": "Furniture", "is_income": False, "parent_category_id": WORK_ID},
+        {
+            "label": "Work materials",
+            "is_income": False,
+            "parent_category_id": WORK_ID,
+        },  # de: Arbeitsutensilien
+        {"label": "Mailings", "is_income": False, "parent_category_id": WORK_ID},
+        {"label": "Workwear", "is_income": False, "parent_category_id": WORK_ID},
+        {"label": "Travel expenses", "is_income": False, "parent_category_id": WORK_ID},
     ]
 
     transport_cats = [
-        ("Public transport", False),
-        ("Gas", False),  # de: Tanken
-        ("Car repairs", False),
-        ("Car care", False),
-        ("Vehicle tax", False),  # de: KFZ-Steuer
+        {
+            "label": "Public transport",
+            "is_income": False,
+            "parent_category_id": TRANSPORT_ID,
+        },
+        {
+            "label": "Gas",
+            "is_income": False,
+            "parent_category_id": TRANSPORT_ID,
+        },  # de: Tanken
+        {
+            "label": "Car repairs",
+            "is_income": False,
+            "parent_category_id": TRANSPORT_ID,
+        },
+        {"label": "Car care", "is_income": False, "parent_category_id": TRANSPORT_ID},
+        {
+            "label": "Vehicle tax",
+            "is_income": False,
+            "parent_category_id": TRANSPORT_ID,
+        },  # de: KFZ-Steuer
     ]
 
     education_cats = [
-        ("Semester fees", False),
-        ("school fees", False),
-        ("Travel expenses", False),
-        ("Teaching aids", False),
-        ("Course fees", False),
+        {
+            "label": "Semester fees",
+            "is_income": False,
+            "parent_category_id": EDUCATION_ID,
+        },
+        {
+            "label": "school fees",
+            "is_income": False,
+            "parent_category_id": EDUCATION_ID,
+        },
+        {
+            "label": "Travel expenses",
+            "is_income": False,
+            "parent_category_id": EDUCATION_ID,
+        },
+        {
+            "label": "Teaching aids",
+            "is_income": False,
+            "parent_category_id": EDUCATION_ID,
+        },
+        {
+            "label": "Course fees",
+            "is_income": False,
+            "parent_category_id": EDUCATION_ID,
+        },
     ]
 
     insurance_cats = [
-        ("Pension insurance", False),
-        ("Health insurance", False),
-        ("Liability insurance", False),
-        ("Car insurance", False),
-        ("Occupational disability insurance", False),
-        ("Accident insurance", False),
-        ("Term life insurance", False),  # de: Risikolebensversicherung
-        ("Home insurance", False),
-        ("Life insurance", False),
+        {
+            "label": "Pension insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Health insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Liability insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Car insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Occupational disability insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Accident insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Term life insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },  # de: Risikolebensversicherung
+        {
+            "label": "Home insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
+        {
+            "label": "Life insurance",
+            "is_income": False,
+            "parent_category_id": INSURANCE_ID,
+        },
     ]
 
     finance_cats = [
-        ("Loans", False),
-        ("Pocket Money", False),
-        ("Alimony", False),
-        ("Reserves", False),
+        {"label": "Loans", "is_income": False, "parent_category_id": FINANCE_ID},
+        {"label": "Pocket Money", "is_income": False, "parent_category_id": FINANCE_ID},
+        {"label": "Alimony", "is_income": False, "parent_category_id": FINANCE_ID},
+        {"label": "Reserves", "is_income": False, "parent_category_id": FINANCE_ID},
     ]
 
     other_cats = [
-        ("Medical Expenses", False),
-        ("Bank Fees", False),
-        ("Tax consultancy", False),
-        ("Tax", False),
+        {
+            "label": "Medical Expenses",
+            "is_income": False,
+            "parent_category_id": OTHER_ID,
+        },
+        {"label": "Bank Fees", "is_income": False, "parent_category_id": OTHER_ID},
+        {
+            "label": "Tax consultancy",
+            "is_income": False,
+            "parent_category_id": OTHER_ID,
+        },
+        {"label": "Tax", "is_income": False, "parent_category_id": OTHER_ID},
     ]
 
-    data = {
-        "Income": income_cats,
-        "Work": work_cats,
-        "Housing": housing_cats,
-        "Houshold": houshold_cats,
-        "Insurance": insurance_cats,
-        "Transport": transport_cats,
-        "Financing": finance_cats,
-        "Education": education_cats,
-        "Leisure": leisure_cats,
-        "Others": other_cats,
-    }
-
-    for categorie, subcategories in data.items():
-
-        result = (
-            db_session.query(TransactionCategory).filter_by(label=categorie).first()
-        )
-        commit = False
-
-        if not result:
-            transaction_category = TransactionCategory(label=categorie)
-            commit = True
-            db_session.add(transaction_category)
-        else:
-            transaction_category = result
-
-        for subcategory_data in subcategories:
-
-            subcategory_label = subcategory_data[0]
-            result_sub = (
-                db_session.query(TransactionSubcategory)
-                .filter_by(label=subcategory_label)
-                .first()
-            )
-
-            if result_sub:
-                continue
-
-            commit = True
-            parent_category = transaction_category.id
-            new_sub = TransactionSubcategory(
-                label=subcategory_label,
-                parent_category_id=parent_category,
-                is_income=subcategory_data[1],
-            )
-            db_session.add(new_sub)
-
-        if commit:
-            db_session.commit()
+    return (
+        income_cats
+        + houshold_cats
+        + housing_cats
+        + finance_cats
+        + other_cats
+        + insurance_cats
+        + education_cats
+        + work_cats
+        + leisure_cats
+        + transport_cats
+    )
 
 
-if __name__ == "__main__":
-    create_categories()
+def _get_category_list():
+    return [
+        {"id": INCOME_ID, "label": "Income"},
+        {"id": WORK_ID, "label": "Work"},
+        {"id": HOUSING_ID, "label": "Housing"},
+        {"id": HOUSHOLD_ID, "label": "Houshold"},
+        {"id": INSURANCE_ID, "label": "Insurance"},
+        {"id": TRANSPORT_ID, "label": "Transport"},
+        {"id": FINANCE_ID, "label": "Financing"},
+        {"id": EDUCATION_ID, "label": "Education"},
+        {"id": LEISURE_ID, "label": "Leisure"},
+        {"id": OTHER_ID, "label": "Others"},
+    ]
+
+
+def create_categories(db_session: Session = None):
+    """Create transaction categories."""
+
+    subcategory_list = _get_subcategory_list()
+    category_list = _get_category_list()
+
+    if db_session:
+        _add_category_via_session(db_session, category_list, subcategory_list)
+    else:
+        _add_category_via_alembic_operator(category_list, subcategory_list)
+
+
+def _add_category_via_alembic_operator(category_list, subcategory_list):
+    category_table = table(
+        "transactions_categories", column("id", sa.Integer), column("label", sa.String)
+    )
+
+    subcategory_table = table(
+        "transactions_subcategories",
+        column("label", sa.String),
+        column("is_income", sa.Boolean),
+        column("parent_category_id", sa.Integer),
+    )
+
+    op.bulk_insert(category_table, category_list)
+    op.bulk_insert(subcategory_table, subcategory_list)
+
+
+def _add_category_via_session(db_session, category_list, subcategory_list):
+    def create_subcategory_model(subcategory):
+        return models.TransactionSubcategory(**subcategory)
+
+    def create_category_model(category):
+        return models.TransactionCategory(**category)
+
+    subcategory = list(map(create_subcategory_model, subcategory_list))
+    category = list(map(create_category_model, category_list))
+
+    db_session.add_all(category + subcategory)
+    db_session.commit()
