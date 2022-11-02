938038d634af5e1af8e368fa76c7e9db610364c1
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-02 19:42:11 +0100

    fastapi users support added

--- app/database.py
+++ app/database.py
@@ -1,10 +1,36 @@
-from sqlalchemy import create_engine
+from typing import AsyncGenerator, List
+from fastapi import Depends
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 from .config import settings
+from fastapi_users.db import (
+    SQLAlchemyBaseUserTableUUID,
+    SQLAlchemyUserDatabase,
+    SQLAlchemyBaseOAuthAccountTableUUID,
+)
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.db_username}:{settings.db_password}@{settings.db_host}:{settings.db_port}/{settings.db_name}"
+SQLALCHEMY_DATABASE_URL = settings.db_url
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL)
 Base = declarative_base()
+
+
+async_session_maker = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+
+
+class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
+    pass
+
+
+class User(SQLAlchemyBaseUserTableUUID, Base):
+    oauth_accounts: List[OAuthAccount] = relationship("OAuthAccount", lazy="joined")
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
+
+
+async def get_user_db(session: AsyncSession = Depends(get_async_session)):
+    yield SQLAlchemyUserDatabase(session, User, OAuthAccount)
