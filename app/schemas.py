27e45ae0522057f7e67a6e8b2401145076581ec3
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-02 19:42:51 +0100

    fastapi schemas update

--- app/schemas.py
+++ app/schemas.py
@@ -1,55 +1,28 @@
-import re
-from datetime import datetime, date
-from pydantic import BaseModel, EmailStr, validator
-from typing import Optional, List
+import uuid
+
+from datetime import datetime
+from pydantic import BaseModel
+from typing import Optional
+from fastapi_users import schemas
 
 from pydantic.types import constr
 
 
-def username_validator(username: str):
-    assert re.match(r"^\w+$", username), "must be alphanumeric"
-    return username
+class UserRead(schemas.BaseUser[uuid.UUID]):
+    pass
 
 
-def _validate_username():
-    return validator("username", allow_reuse=True)(username_validator)
+class UserCreate(schemas.BaseUserCreate):
+    pass
+
+
+class UserUpdate(schemas.BaseUserUpdate):
+    pass
 
 
 class Base(BaseModel):
     class Config:
         orm_mode = True
-
-
-class UserBase(Base):
-    username: constr(to_lower=True)
-    email: EmailStr
-
-    _validate_username = _validate_username()
-
-
-class UserCreate(UserBase):
-    password: str
-
-
-class UserData(UserBase):
-    id: int
-
-
-class UserUpdate(Base):
-    username: Optional[str] = ""
-    email: Optional[EmailStr] = ""
-    password: Optional[str] = None
-
-    _validate_username = _validate_username()
-
-
-class Token(BaseModel):
-    access_token: str
-    token_type: str
-
-
-class TokenData(BaseModel):
-    id: Optional[str]
 
 
 class AccountUpdate(Base):
