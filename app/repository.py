ab939b0931a050f0913395dcfd4dfcd5bdef304c
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-08 07:49:35 +0100

    update to new db middleware

--- app/repository.py
+++ app/repository.py
@@ -2,32 +2,32 @@ import uuid
 from sqlalchemy import or_, update as sql_update
 from sqlalchemy.future import select
 from . import models
-from .database import db
 from datetime import datetime
+from fastapi_async_sqlalchemy import db
 
 
 async def get_all(cls: models):
     q = select(cls)
-    result = await db._session.execute(q)
+    result = await db.session.execute(q)
     return result.all()
 
 
 async def filter_by(cls: models, attribute: str, value: str):
     query = select(cls).where(getattr(cls, attribute) == value)
-    result = await db._session.execute(query)
+    result = await db.session.execute(query)
     return result.scalars().all()
 
 
 async def get(cls: models, id: int):
     query = select(cls).where(cls.id == id)
-    result = await db._session.execute(query)
+    result = await db.session.execute(query)
     db_object = result.scalar_one_or_none()
     return db_object
 
 
 async def get_user(id: uuid):
     query = select(models.User).where(models.User == id)
-    result = await db._session.execute(query)
+    result = await db.session.execute(query)
     return result.scalar_one_or_none()
 
 
@@ -47,7 +47,7 @@ async def get_transactions_from_period(
         .filter(account_id == transaction.account_id)
     )
 
-    result = await db._session.execute(query)
+    result = await db.session.execute(query)
     return result.scalars().all()
 
 
@@ -59,17 +59,17 @@ async def get_scheduled_transactions_for_date(date: datetime):
         .filter(or_(ts.date_end == None, ts.date_end >= date))
     )
 
-    result = await db._session.execute(query)
+    result = await db.session.execute(query)
 
     return result.scalars().all()
 
 
 async def save(obj):
     if isinstance(obj, list):
-        db._session.add_all(obj)
+        db.session.add_all(obj)
         return
 
-    db._session.add(obj)
+    db.session.add(obj)
 
 
 async def update(cls: models, id: int, **kwargs):
@@ -79,19 +79,19 @@ async def update(cls: models, id: int, **kwargs):
         .values(**kwargs)
         .execution_options(synchronize_session="fetch")
     )
-    await db._session.execute(query)
+    await db.session.execute(query)
 
 
 async def delete(obj: models) -> None:
-    await db._session.delete(obj)
+    await db.session.delete(obj)
 
 
 async def refresh(obj: models):
     print("\033[2;31;43m refresh method called, check for odd behaviour \033[0;0m")
-    return db._session.refresh(obj)
+    return db.session.refresh(obj)
 
 
 async def refresh_all(object_list: models) -> None:
     print("\033[2;31;43m refresh_all method called, check for odd behaviour \033[0;0m")
     for obj in object_list:
-        db._session.refresh(obj)
+        db.session.refresh(obj)
