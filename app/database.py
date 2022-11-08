9392788cf6333805329d335e229efc5b2a47c682
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-08 07:48:48 +0100

    using a new lib for asyn sessions

--- app/database.py
+++ app/database.py
@@ -1,33 +1,17 @@
 from typing import List
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.orm import relationship
+from fastapi_async_sqlalchemy import db
 from .config import settings
 from fastapi_users.db import (
     SQLAlchemyBaseUserTableUUID,
     SQLAlchemyUserDatabase,
     SQLAlchemyBaseOAuthAccountTableUUID,
 )
-from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 
 SQLALCHEMY_DATABASE_URL = settings.db_url
 
 Base = declarative_base()
-
-
-class AsyncDatabaseSession:
-    def __init__(self):
-        self._session = None
-        self._engine = None
-
-    def init(self):
-        self._engine = create_async_engine(
-            SQLALCHEMY_DATABASE_URL,
-            future=True,
-        )
-        self._session = sessionmaker(self._engine, class_=AsyncSession)()
-
-
-db = AsyncDatabaseSession()
 
 
 class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
@@ -39,4 +23,4 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 
 
 async def get_user_db():
-    yield SQLAlchemyUserDatabase(db._session, User, OAuthAccount)
+    yield SQLAlchemyUserDatabase(db.session, User, OAuthAccount)
