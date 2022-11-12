24ebd7e54a875d3b9bdf8901edca6b9608ad215f
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-12 21:19:20 +0100

    added own database class

--- app/database.py
+++ app/database.py
@@ -1,7 +1,9 @@
+import sys
 from typing import List
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import relationship
-from fastapi_async_sqlalchemy import db
 from .config import settings
 from fastapi_users.db import (
     SQLAlchemyBaseUserTableUUID,
@@ -9,9 +11,29 @@ from fastapi_users.db import (
     SQLAlchemyBaseOAuthAccountTableUUID,
 )
 
-SQLALCHEMY_DATABASE_URL = settings.db_url
-
 Base = declarative_base()
+
+
+class Database:
+    def __init__(self, url):
+        self.session = None
+        self.engine = None
+        self.url = url
+
+    async def create_all(self):
+        async with self.engine.begin() as conn:
+            await conn.run_sync(Base.metadata.create_all)
+
+    async def init(self):
+        # closes connections if a session is created,
+        # so as not to create repeated connections
+        if self.session:
+            await self.session.close()
+
+        self.engine = create_async_engine(self.url, future=True)
+        self.session = sessionmaker(
+            self.engine, expire_on_commit=False, class_=AsyncSession
+        )()
 
 
 class OAuthAccount(SQLAlchemyBaseOAuthAccountTableUUID, Base):
@@ -24,3 +46,12 @@ class User(SQLAlchemyBaseUserTableUUID, Base):
 
 async def get_user_db():
     yield SQLAlchemyUserDatabase(db.session, User, OAuthAccount)
+
+
+SQLALCHEMY_DATABASE_URL = settings.db_url
+
+
+if "pytest" in sys.modules:
+    SQLALCHEMY_DATABASE_URL = settings.test_db_url
+
+db: Database = Database(SQLALCHEMY_DATABASE_URL)
