9356019388a794ad4e6ad639f982992519cbc3c9
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-11-12 21:24:18 +0100

    just take it as it is

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,43 +1,41 @@
-import anyio
 import pytest
-from httpx import AsyncClient
-from fastapi_async_sqlalchemy import SQLAlchemyMiddleware
+import asyncio
 from app import events
 from app.config import settings
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from app.main import app
-from app.database import Base
+from app.database import Base, db
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.pool import NullPool
+from httpx import AsyncClient
 
-engine = create_async_engine(settings.test_db_url, poolclass=NullPool)
-app.add_middleware(SQLAlchemyMiddleware, db_url=settings.test_db_url)
+
+engine = create_async_engine(settings.test_db_url)
 async_session = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 
-
-async def init_models():
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)
-
-    await events.create_categories(async_session())
-
-
-anyio.run(init_models)
 
 pytestmark = pytest.mark.anyio
 
 
 @pytest.fixture
-async def session() -> AsyncSession:
-    async with async_session() as session:
-        yield session
+async def client() -> AsyncClient:
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
 
 
 @pytest.fixture
-async def client(session):
-    async with AsyncClient(app=app, base_url="http://test") as client:
-        yield client
+async def session():
+    await db.init()
+    async with db.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)
+
+    await events.create_categories(db.session)
+    yield db.session
+
+
+@pytest.fixture
+def anyio_backend():
+    return "asyncio"
 
 
 from .fixtures import *
