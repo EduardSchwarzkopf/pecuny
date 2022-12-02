044930dbf6dacb337b77282d480327352428fed1
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-02 07:47:33 +0100

    changed transport to cookies

--- app/routers/users.py
+++ app/routers/users.py
@@ -5,7 +5,7 @@ from fastapi import Depends, Request, APIRouter, status, Response, HTTPException
 from fastapi_users import BaseUserManager, FastAPIUsers, UUIDIDMixin
 from fastapi_users.authentication import (
     AuthenticationBackend,
-    BearerTransport,
+    CookieTransport,
     JWTStrategy,
 )
 from fastapi_users.db import SQLAlchemyUserDatabase
@@ -42,7 +42,7 @@ async def get_user_manager(user_db: SQLAlchemyUserDatabase = Depends(get_user_db
     yield UserManager(user_db)
 
 
-bearer_transport = BearerTransport(tokenUrl="auth/jwt/login")
+cookie_transport = CookieTransport(cookie_max_age=60)
 
 
 def get_jwt_strategy() -> JWTStrategy:
@@ -51,7 +51,7 @@ def get_jwt_strategy() -> JWTStrategy:
 
 auth_backend = AuthenticationBackend(
     name="jwt",
-    transport=bearer_transport,
+    transport=cookie_transport,
     get_strategy=get_jwt_strategy,
 )
 
