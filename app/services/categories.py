2bd34fd336d47b9c2b827f2bb53b16b3649f61b0
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2022-12-24 08:49:00 +0100

    category service added

---
+++ app/services/categories.py
@@ -0,0 +1,49 @@
+from typing import List
+from .. import models, schemas, repository as repo
+
+
+async def get_categories(
+    current_user: models.User,
+) -> List[models.TransactionCategory]:
+    return await repo.get_all(models.TransactionCategory)
+
+
+async def get_category(
+    current_user: models.User, category_id: int
+) -> models.TransactionCategory:
+
+    category = await repo.get(models.TransactionCategory, category_id)
+
+    if category and (category.user_id is None or category.user_id == current_user.id):
+        return category
+
+    return None
+
+
+# async def create_category(user: models.User, category: schemas.TransactionCategory) -> models.TransactionCategory:
+
+#     db_category = models.TransactionCategory(user=user, **category.dict())
+#     await repo.save(db_category)
+#     return db_category
+
+
+# async def update_category(
+#     current_user: models.User, category_id, category: schemas.TransactionCategoryData
+# ) -> models.TransactionCategory:
+
+#     db_category = await repo.get(models.TransactionCategory, category_id)
+#     if db_category.user_id == current_user.id:
+#         await repo.update(models.TransactionCategory, db_category.id, **category.dict())
+#         return db_category
+
+#     return None
+
+
+# async def delete_category(current_user: models.User, category_id: int) -> bool:
+
+#     category = await repo.get(models.TransactionCategory, category_id)
+#     if category and category.user_id == current_user.id:
+#         await repo.delete(category)
+#         return True
+
+#     return None
