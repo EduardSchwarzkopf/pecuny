25d6e680d47b01ab3c0740dae0e5d7c942e4a75a
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-01-26 07:18:36 +0100

    creation added

--- app/services/scheduled_transactions.py
+++ app/services/scheduled_transactions.py
@@ -4,21 +4,33 @@ from app import models, schemas, repository as repo
 async def create_scheduled_transaction(
     user: models.User,
     transaction_information: schemas.ScheduledTransactionInformationCreate,
-) -> models.Transaction:
+) -> models.TransactionScheduled:
 
     account = await repo.get(models.Account, transaction_information.account_id)
 
     if user.id.bytes != account.user_id.bytes:
         return None
 
+    offset_account_id = transaction_information.offset_account_id
+
+    if offset_account_id:
+        offset_account = await repo.get(models.Account, offset_account_id)
+
+        if user.id.bytes != offset_account.user_id.bytes:
+            raise Exception(
+                f"User[id: {user.id}] not allowed to access offset_account[id: {transaction_information.offset_account_id}]"
+            )
+
     db_transaction_information = models.TransactionInformation()
     db_transaction_information.add_attributes_from_dict(transaction_information.dict())
 
-    # TODO: Frequency
-    # TODO: date_start, date_end
-    # TODO: Offset account
     transaction = models.TransactionScheduled(
-        information=db_transaction_information, account_id=account.id
+        frequency_id=transaction_information.frequency_id,
+        date_start=transaction_information.date_start,
+        date_end=transaction_information.date_end,
+        information=db_transaction_information,
+        account_id=account.id,
+        offset_account_id=offset_account_id,
     )
 
     await repo.save([transaction, db_transaction_information])
