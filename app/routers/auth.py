da6440f8cfdd029380d9b57ad15c6445e7ee5a81
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-12 08:24:11 +0200

    added login logic

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,18 @@
 from app import templates
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, HTTPException, Request, status
 from fastapi.responses import HTMLResponse, RedirectResponse
-from typing import Optional
+from app.models import User
+from fastapi.security import OAuth2PasswordRequestForm
+from app.routers.api.users import (
+    fastapi_users,
+    get_user_manager,
+    auth_backend,
+    UserManager,
+    JWTStrategy,
+)
+
+current_active_user = fastapi_users.current_user(optional=True)
 
 router = APIRouter()
 template_prefix = "pages/auth"
@@ -11,9 +21,36 @@ template_prefix = "pages/auth"
 @router.get(
     path="/login", tags=["Pages", "Authentication"], response_class=HTMLResponse
 )
-async def get_login(request: Request):
+async def get_login(request: Request, user: User = Depends(current_active_user)):
+    print(user)
+    if user:
+        return RedirectResponse("/")
+
     context = {"request": request}
     return templates.TemplateResponse(f"{template_prefix}/login.html", context)
+
+
+@router.post(
+    "/login",
+    response_class=RedirectResponse,
+)
+async def login(
+    request: Request,
+    credentials: OAuth2PasswordRequestForm = Depends(),
+    user_manager: UserManager = Depends(get_user_manager),
+    strategy: JWTStrategy = Depends(auth_backend.get_strategy),
+):
+    user = await user_manager.authenticate(credentials)
+
+    if user is None or not user.is_active:
+        return RedirectResponse("/login?credentials=False", status_code=302)
+
+    if not user.is_verified:
+        return RedirectResponse("/login?verified=False", status_code=302)
+
+    response = await auth_backend.login(strategy, user)
+    await user_manager.on_after_login(user, request, response)
+    return response
 
 
 @router.get(
