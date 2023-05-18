3453f5da952bd38e5cb51cf8fbba7b91df0bc4e7
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-18 17:30:02 +0200

    form schema added

--- app/schemas.py
+++ app/schemas.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from typing import Optional, List, Dict, Any
 from fastapi_users import schemas
+from fastapi import Form
 
 from pydantic.types import constr
 
@@ -19,6 +20,13 @@ class UserRead(schemas.BaseUser[uuid.UUID]):
 
 class UserCreate(schemas.BaseUserCreate):
     displayname: str
+
+
+class UserCreateForm(BaseModel):
+    email: str = Form(...)
+    displayname: str = Form(...)
+    password: str = Form(...)
+    password_confirm: str = Form(...)
 
 
 class UserUpdate(schemas.BaseUserUpdate):
