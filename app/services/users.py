455d801d692dcf8656ebcbdce4dffa8b4abf28f0
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-18 17:31:43 +0200

    added create logic

--- app/services/users.py
+++ app/services/users.py
@@ -1,7 +1,30 @@
 from app import models, repository as repo
+from app.schemas import UserCreate
+from fastapi_users.exceptions import UserAlreadyExists
+from app.auth_manager import UserManager
 
 
 async def delete_self(current_user: models.User) -> bool:
     await repo.delete(current_user)
 
     return True
+
+
+async def create_user(
+    user_manager: UserManager,
+    email: str,
+    displayname: str,
+    password: str,
+    is_superuser: bool = False,
+):
+    try:
+        return await user_manager.create(
+            UserCreate(
+                email=email,
+                displayname=displayname,
+                password=password,
+                is_superuser=is_superuser,
+            )
+        )
+    except UserAlreadyExists:
+        return None
