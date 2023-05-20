535f1af71787d67a8842a151ad1893400a2d13c6
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-20 15:21:46 +0200

    make use of service class

--- app/routers/api/users.py
+++ app/routers/api/users.py
@@ -1,17 +1,24 @@
 from fastapi import Depends, APIRouter, status, Response, HTTPException
-
 from app import transaction_manager as tm
-from app.services import users as service
+from app.services.users import UserService
 from app.models import User
 from app.auth_manager import current_active_user
 
 router = APIRouter()
 
 
+def get_user_service() -> UserService:
+    return UserService()
+
+
 # override fastapi_users functionality
 @router.delete("/me", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_me(current_user: User = Depends(current_active_user)):
+async def delete_me(
+    service: UserService = Depends(get_user_service),
+    current_user: User = Depends(current_active_user),
+):
     result = await tm.transaction(service.delete_self, current_user)
+
     if result:
         return Response(
             status_code=status.HTTP_204_NO_CONTENT,
