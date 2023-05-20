9ec0e80c0ab71a9fd11ca77288332e208f8e11e7
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-20 12:50:49 +0200

    refactor to class

--- app/services/users.py
+++ app/services/users.py
@@ -1,44 +1,41 @@
-import jwt
 from app import models, repository as repo
-from app.schemas import UserCreate
+from app.schemas import UserCreate, EmailStr
 from fastapi_users import exceptions
 from app.auth_manager import UserManager
 from app.utils.enums import EmailVerificationStatus
 
 
-async def delete_self(current_user: models.User) -> bool:
-    await repo.delete(current_user)
+class UserService:
+    def __init__(self, user_manager: UserManager):
+        self.user_manager = user_manager
 
-    return True
+    async def delete_self(self, current_user: models.User) -> bool:
+        await repo.delete(current_user)
+        return True
 
-
-async def create_user(
-    user_manager: UserManager,
-    email: str,
-    displayname: str,
-    password: str,
-    is_superuser: bool = False,
-) -> bool:
-    try:
-        return await user_manager.create(
-            UserCreate(
-                email=email,
-                displayname=displayname,
-                password=password,
-                is_superuser=is_superuser,
+    async def create_user(
+        self, email: str, displayname: str, password: str, is_superuser: bool = False
+    ) -> bool:
+        try:
+            return await self.user_manager.create(
+                UserCreate(
+                    email=email,
+                    displayname=displayname,
+                    password=password,
+                    is_superuser=is_superuser,
+                )
             )
-        )
-    except exceptions.UserAlreadyExists:
-        return None
+        except exceptions.UserAlreadyExists:
+            return None
 
+    async def verify_email(self, token: str) -> EmailVerificationStatus:
+        try:
+            await self.user_manager.verify(token)
+            return EmailVerificationStatus.VERIFIED
+        except exceptions.InvalidVerifyToken:
+            return EmailVerificationStatus.INVALID_TOKEN
+        except exceptions.UserAlreadyVerified:
+            return EmailVerificationStatus.ALREADY_VERIFIED
 
-async def verify_email(
-    user_manager: UserManager, token: str
-) -> EmailVerificationStatus:
-    try:
-        await user_manager.verify(token)
-        return EmailVerificationStatus.VERIFIED
-    except exceptions.InvalidVerifyToken:
-        return EmailVerificationStatus.INVALID_TOKEN
-    except exceptions.UserAlreadyVerified:
-        return EmailVerificationStatus.ALREADY_VERIFIED
+    async def forgot_password(self, email: EmailStr) -> None:
+        return await self.user_manager.forgot_password(email)
