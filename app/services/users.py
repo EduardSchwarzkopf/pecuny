60b8e184f0d0bcfe53c8b7296190b5ea5371ee52
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-22 07:46:12 +0200

    request verification added

--- app/services/users.py
+++ app/services/users.py
@@ -45,6 +45,14 @@ class UserService:
         except exceptions.UserAlreadyVerified:
             return EmailVerificationStatus.ALREADY_VERIFIED
 
+    async def request_verification(self, user: models.User) -> None:
+        try:
+            await self.user_manager.request_verify(user)
+        except exceptions.UserInactive:
+            print("User inactive")
+        except exceptions.UserAlreadyVerified:
+            print("User already verified")
+
     async def forgot_password(self, email: EmailStr) -> None:
         with contextlib.suppress(exceptions.UserNotExists):
             existing_user = await self.user_manager.get_by_email(email)
