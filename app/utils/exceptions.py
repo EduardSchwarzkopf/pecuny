dfe4d787c39084ffe7f39b0d0360c5ff95a3d586
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-05-31 07:03:27 +0200

    added exceptions

---
+++ app/utils/exceptions.py
@@ -0,0 +1,21 @@
+from fastapi import HTTPException, status
+
+
+class UnauthorizedException(HTTPException):
+    def __init__(self):
+        super().__init__(status.HTTP_401_UNAUTHORIZED, "Unauthorized")
+
+
+class UnauthorizedPageException(HTTPException):
+    def __init__(self):
+        super().__init__(status.HTTP_401_UNAUTHORIZED, "Unauthorized")
+
+
+class ForbiddenException(HTTPException):
+    def __init__(self):
+        super().__init__(status.HTTP_403_FORBIDDEN, "Forbidden")
+
+
+class NotFoundException(HTTPException):
+    def __init__(self):
+        super().__init__(status.HTTP_404_NOT_FOUND, "Not Found")
