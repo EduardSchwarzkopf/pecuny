97fc941c25b792735b8bee744b2a2169057950a4
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-06-09 07:29:17 +0200

    restructure

--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -1,18 +1,19 @@
-from app import templates
-
 from app.models import User
 from fastapi import APIRouter, Request, Depends
 from fastapi.responses import HTMLResponse
 from app.auth_manager import current_active_user
 from app.utils.template_utils import render_template
 
+router = APIRouter(
+    prefix="/dashboard",
+    tags=["Pages", "Dashboard"],
+    default_response_class=HTMLResponse,
+)
 
-router = APIRouter(prefix="/dashboard", tags=["Pages", "Dashboard"])
 
-
-@router.get(path="/", response_class=HTMLResponse)
+@router.get(path="/")
 async def index(
     request: Request,
     user: User = Depends(current_active_user),
 ):
-    return render_template(templates.TemplateResponse("pages/home.html"), request)
+    return render_template("pages/dashboard/index.html", request)
