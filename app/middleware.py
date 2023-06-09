536bfeb312687197b31f8f78889605a6ca4e990f
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-06-09 07:54:30 +0200

    set active links

--- app/middleware.py
+++ app/middleware.py
@@ -1,11 +1,17 @@
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.requests import Request
+from app.routers.dashboard import router as dashboard_router
 
 
 class HeaderLinkMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request: Request, call_next):
+        current_path = request.url.path
+        check = current_path.startswith(dashboard_router.prefix)
         request.state.header_links = [
-            {"url": "/about", "text": "About Us"},
-            {"url": "/contact", "text": "Contact Us"},
+            {
+                "url": dashboard_router.prefix,
+                "text": "Dashboard",
+                "active": current_path.startswith(dashboard_router.prefix),
+            },
         ]
         return await call_next(request)
