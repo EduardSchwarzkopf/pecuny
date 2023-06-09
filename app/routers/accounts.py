ab1c723a05af9de3d17bab44813f95e8d11fcd3c
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-06-09 16:52:42 +0200

    init accounts

---
+++ app/routers/accounts.py
@@ -0,0 +1,35 @@
+from fastapi import Request, Form, Depends
+from fastapi.responses import HTMLResponse, RedirectResponse
+from app.utils import PageRouter
+from app.utils.template_utils import render_template
+from app import schemas, transaction_manager as tm, models
+from app.services import accounts as service
+from app.auth_manager import current_active_user
+
+router = PageRouter(prefix="/accounts", tags=["Accounts"])
+
+
+@router.get("/create-account", response_class=HTMLResponse)
+async def page_create_account_form(
+    request: Request,
+    user: models.User = Depends(current_active_user),
+):
+    return render_template("pages/dashboard/create_account.html", request)
+
+
+@router.post("/create-account")
+async def page_create_account(
+    user: models.User = Depends(current_active_user),
+    label: str = Form(...),
+    description: str = Form(...),
+    balance: float = Form(...),
+):
+    account_data = {"label": label, "description": description, "balance": balance}
+
+    account = schemas.Account(**account_data)
+
+    response = await tm.transaction(service.create_account, user, account)
+
+    return RedirectResponse(
+        router.url_path_for("page_create_account_form"), status_code=302
+    )
