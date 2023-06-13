5359a7879b11e5363f3bad84d77ae476dfdbd43e
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-06-13 14:33:23 +0200

    new colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-const primary = "#649d9d";
-const primaryLight = "#8cbcba";
-const primaryDark = "#4a8182";
+const primary = "#ff9c5f";
+const primaryLight = "#ffaf7e";
+const primaryDark = "#fe7839";
 
 module.exports = {
     content: ["templates/**/*.html"],
@@ -12,17 +12,47 @@ module.exports = {
                     DEFAULT: primary,
                     light: primaryLight,
                     dark: primaryDark,
-                    50: "#f4f9f9",
-                    100: "#dcebea",
-                    200: "#a7cecb",
-                    300: "#8cbcba",
-                    400: "#649d9d",
-                    500: "#4a8182",
-                    600: "#3a6667",
-                    700: "#315354",
-                    800: "#2a4445",
-                    900: "#263b3b",
-                    950: "#122021",
+                    50: "#fff5ed",
+                    100: "#ffe7d4",
+                    200: "#ffaf7e",
+                    300: "#ff9c5f",
+                    400: "#fe7839",
+                    500: "#fc5413",
+                    600: "#ed3909",
+                    700: "#c52809",
+                    800: "#9c2110",
+                    900: "#7e1e10",
+                    950: "#440c06",
+                },
+                info: {
+                    light: "#c7def0",
+                    DEFAULT: "#5ca2d4",
+                    dark: "#3886bf",
+                    hover: "#95c1e4",
+                },
+                danger: {
+                    light: "#fdced5",
+                    DEFAULT: "#f14668",
+                    dark: "#dd214f",
+                    hover: "#f8748b",
+                },
+                success: {
+                    light: "#dcfce6",
+                    DEFAULT: "#85f0aa",
+                    dark: "#49df7d",
+                    hover: "#bbf7cf",
+                },
+                warning: {
+                    light: "#fffaeb",
+                    DEFAULT: "#ffe08a",
+                    dark: "#ffc94a",
+                    hover: "#fff0c6",
+                },
+                dark: {
+                    light: "gray-50",
+                    DEFAULT: "gray-800",
+                    dark: "gray-300",
+                    hover: "gray-200",
                 },
             },
         },
