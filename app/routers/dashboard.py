673bc73eb25c11fc38bc5fc49f2b525dd26a558d
48969167 <48969167+EduardSchwarzkopf@users.noreply.github.com>
2023-07-09 07:51:44 +0200

    redirect

--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -4,9 +4,7 @@ from fastapi import Request, Depends
 
 from app.auth_manager import current_active_user
 from app.utils import PageRouter
-from app.utils.template_utils import render_template
-from app.services import accounts as service
-from app.config import settings
+from fastapi.responses import RedirectResponse
 
 
 router = PageRouter(tags=["Dashboard"], prefix="/dashboard")
@@ -17,12 +15,5 @@ async def page_dashboard(
     request: Request,
     user: User = Depends(current_active_user),
 ):
-    account_list = await service.get_accounts(user)
-    return render_template(
-        "pages/dashboard/page_multiple_accounts.html",
-        request,
-        {
-            "account_list": account_list,
-            "max_allowed_accounts": settings.max_allowed_accounts,
-        },
-    )
+    # TODO: Create a usueful dashboard page
+    return RedirectResponse(f"{router.prefix}/accounts", status_code=302)
