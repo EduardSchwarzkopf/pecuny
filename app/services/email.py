c10d9a8cb1d03099543e6557c3661ecfc8e3d550
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-14 22:09:30 +0100

    add register exceptions

--- app/services/email.py
+++ app/services/email.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi_mail import ConnectionConfig, FastMail, MessageSchema, MessageType
+from fastapi_mail.errors import ConnectionErrors
 from app.schemas import EmailSchema
 from starlette.responses import JSONResponse
 from app.config import settings
@@ -8,18 +9,21 @@ from app.logger import get_logger
 
 log = get_logger(__name__)
 
-conf = ConnectionConfig(
-    MAIL_USERNAME=settings.mail_username,
-    MAIL_PASSWORD=settings.mail_password,
-    MAIL_FROM=settings.mail_from,
-    MAIL_PORT=settings.mail_port,
-    MAIL_SERVER=settings.mail_server,
-    MAIL_STARTTLS=False,
-    MAIL_SSL_TLS=True,
-    USE_CREDENTIALS=True,
-    VALIDATE_CERTS=True,
-    TEMPLATE_FOLDER="templates",
-)
+try:
+    conf = ConnectionConfig(
+        MAIL_USERNAME=settings.mail_username,
+        MAIL_PASSWORD=settings.mail_password,
+        MAIL_FROM=settings.mail_from,
+        MAIL_PORT=settings.mail_port,
+        MAIL_SERVER=settings.mail_server,
+        MAIL_STARTTLS=False,
+        MAIL_SSL_TLS=True,
+        USE_CREDENTIALS=True,
+        VALIDATE_CERTS=True,
+        TEMPLATE_FOLDER="templates",
+    )
+except ConnectionError:
+    log.error("No connection established")
 
 
 async def _send(email: EmailSchema, subject: str, template_name: str) -> JSONResponse:
