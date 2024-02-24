9775e6f8575e7e1ed005cb2069c0c77d7d1c0cc3
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:13:47 +0100

    update linting

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,16 +1,14 @@
 from fastapi import Depends, Request
-from fastapi.exceptions import HTTPException
 from fastapi.responses import RedirectResponse
-from starlette import status
 from starlette_wtf import csrf_protect
 
 from app import schemas
 from app.auth_manager import current_active_user
 from app.models import User
+from app.services.users import UserService
 from app.utils import PageRouter
 from app.utils.enums import FeedbackType
 from app.utils.template_utils import render_template, set_feedback
-from app.services.users import UserService
 
 router = PageRouter(tags=["Users"], prefix="/users")
 
@@ -21,6 +19,17 @@ async def page_user_settings_form(
     request: Request,
     user: User = Depends(current_active_user),
 ):
+    """
+    Renders the user settings form page.
+
+    Args:
+        request: The request object.
+        user: The current active user.
+
+    Returns:
+        TemplateResponse: The rendered user settings form page.
+    """
+
     form = schemas.UpdateUserForm(request, data=user.__dict__)
 
     return render_template(
@@ -36,6 +45,18 @@ async def page_user_settings(
     request: Request,
     user: User = Depends(current_active_user),
 ):
+    """
+    Handles the user settings form submission.
+
+    Args:
+        request: The request object.
+        user: The current active user.
+
+    Returns:
+        Union[TemplateResponse, RedirectResponse]:
+            The rendered user settings form page or a redirect response.
+    """
+
     form = await schemas.UpdateUserForm.from_formdata(request)
     if not await form.validate_on_submit():
         for field in form.errors:
