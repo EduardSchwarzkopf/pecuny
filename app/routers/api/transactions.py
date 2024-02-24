a248f2195b7b284ae7bf95a5a211be7fe02137cb
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:40:56 +0100

    add docstrings

--- app/routers/api/transactions.py
+++ app/routers/api/transactions.py
@@ -22,6 +22,22 @@ async def api_get_transactions(
     date_end: datetime,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Retrieves a list of transactions.
+
+    Args:
+        account_id: The ID of the account.
+        date_start: The start date for filtering transactions.
+        date_end: The end date for filtering transactions.
+        current_user: The current active user.
+
+    Returns:
+        List[ResponseModel]: A list of transaction information.
+
+    Raises:
+        HTTPException: If the account is not found.
+    """
+
     return await service.get_transaction_list(
         current_user, account_id, date_start, date_end
     )
@@ -32,6 +48,20 @@ async def api_get_transaction(
     transaction_id: int,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Retrieves a transaction by ID.
+
+    Args:
+        transaction_id: The ID of the transaction.
+        current_user: The current active user.
+
+    Returns:
+        ResponseModel: The retrieved transaction information.
+
+    Raises:
+        HTTPException: If the transaction is not found.
+    """
+
     transaction = await tm.transaction(
         service.get_transaction, current_user, transaction_id
     )
@@ -49,6 +79,20 @@ async def api_create_transaction(
     transaction_information: schemas.TransactionInformationCreate,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Creates a new transaction.
+
+    Args:
+        transaction_information: The information of the transaction.
+        current_user: The current active user.
+
+    Returns:
+        ResponseModel: The created transaction information.
+
+    Raises:
+        HTTPException: If the transaction is not created.
+    """
+
     transaction = await tm.transaction(
         service.create_transaction, current_user, transaction_information
     )
@@ -67,6 +111,21 @@ async def api_update_transaction(
     transaction_information: schemas.TransactionInformtionUpdate,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Updates a transaction.
+
+    Args:
+        transaction_id: The ID of the transaction.
+        transaction_information: The updated transaction information.
+        current_user: The current active user.
+
+    Returns:
+        ResponseModel: The updated transaction information.
+
+    Raises:
+        HTTPException: If the transaction is not found.
+    """
+
     transaction = await tm.transaction(
         service.update_transaction,
         current_user,
@@ -86,6 +145,20 @@ async def api_update_transaction(
 async def api_delete_account(
     transaction_id: int, current_user: User = Depends(current_active_user)
 ):
+    """
+    Deletes a transaction.
+
+    Args:
+        transaction_id: The ID of the transaction.
+        current_user: The current active user.
+
+    Returns:
+        Response: A response indicating the success or failure of the deletion.
+
+    Raises:
+        HTTPException: If the transaction is not found.
+    """
+
     result = await tm.transaction(
         service.delete_transaction, current_user, transaction_id
     )
