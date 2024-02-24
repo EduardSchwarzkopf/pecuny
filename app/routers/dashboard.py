33cab751e96e65e6333aef601db99a7d5b6293cd
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:22:48 +0100

    linting

--- app/routers/dashboard.py
+++ app/routers/dashboard.py
@@ -1,19 +1,27 @@
-from app.models import User
-from fastapi import Request, Depends
-
-
-from app.auth_manager import current_active_user
-from app.utils import PageRouter
+from fastapi import Depends
 from fastapi.responses import RedirectResponse
 
+from app.auth_manager import current_active_user
+from app.models import User
+from app.utils import PageRouter
 
 router = PageRouter(tags=["Dashboard"], prefix="/dashboard")
 
 
 @router.get("/")
 async def page_dashboard(
-    request: Request,
-    user: User = Depends(current_active_user),
+    user: User = Depends(current_active_user),  # pylint: disable=unused-argument
 ):
+    """
+    Redirects to the dashboard page.
+
+    Args:
+        user: The current active user.
+
+    Returns:
+        RedirectResponse: A redirect response to the accounts page.
+    """
+
     # TODO: Create a usueful dashboard page
+
     return RedirectResponse(f"{router.prefix}/accounts", status_code=302)
