96ebe29877b506ca08ea4dba42a5f036b6021cc3
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:42:02 +0100

    add docstrings

--- app/routers/api/scheduled_transactions.py
+++ app/routers/api/scheduled_transactions.py
@@ -24,6 +24,22 @@ async def api_get_transactions(
     date_end: datetime,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Retrieves a list of transactions.
+
+    Args:
+        account_id: The ID of the account.
+        date_start: The start date for filtering transactions.
+        date_end: The end date for filtering transactions.
+        current_user: The current active user.
+
+    Returns:
+        List[response_model]: A list of transaction information.
+
+    Raises:
+        HTTPException: If the account is not found.
+    """
+
     return await service.get_transaction_list(
         current_user, account_id, date_start, date_end
     )
@@ -34,6 +50,20 @@ async def api_get_transaction(
     transaction_id: int,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Retrieves a scheduled transaction by ID.
+
+    Args:
+        transaction_id: The ID of the scheduled transaction.
+        current_user: The current active user.
+
+    Returns:
+        ResponseModel: The retrieved scheduled transaction information.
+
+    Raises:
+        HTTPException: If the scheduled transaction is not found.
+    """
+
     transaction = await service.get_transaction(current_user, transaction_id)
 
     if transaction:
@@ -49,6 +79,20 @@ async def api_create_transaction(
     transaction_information: schemas.ScheduledTransactionInformationCreate,
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Creates a new scheduled transaction.
+
+    Args:
+        transaction_information: The information of the scheduled transaction.
+        current_user: The current active user.
+
+    Returns:
+        ResponseModel: The created scheduled transaction information.
+
+    Raises:
+        HTTPException: If the scheduled transaction is not created.
+    """
+
     transaction = await tm.transaction(
         service.create_scheduled_transaction, current_user, transaction_information
     )
