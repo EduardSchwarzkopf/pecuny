d74950bb2355622eebc6937592d9ae075a53e225
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 19:31:06 +0100

    add docstring

--- app/utils/account_utils.py
+++ app/utils/account_utils.py
@@ -5,7 +5,6 @@ from fastapi import Request
 from app import models
 from app.config import settings
 from app.services import accounts as service
-from app.utils.log_messages import ACCOUNT_USER_ID_MISMATCH
 from app.utils.template_utils import render_template
 
 
@@ -26,6 +25,21 @@ def calculate_total_balance(account_list: List[models.Account]) -> float:
 async def get_account_list_template(
     user: models.User, template_name: str, request: Request
 ):
+    """
+    Renders an account list template.
+
+    Args:
+        user: The user object.
+        template_name: The name of the template.
+        request: The request object.
+
+    Returns:
+        The rendered template.
+
+    Raises:
+        None
+    """
+
     account_list = await service.get_accounts(user)
     total_balance = calculate_total_balance(account_list)
 
