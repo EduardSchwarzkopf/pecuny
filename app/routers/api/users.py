ead63f51040fba8dfcd998be760f18ac76960caf
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:37:31 +0100

    add docstrings

--- app/routers/api/users.py
+++ app/routers/api/users.py
@@ -10,6 +10,13 @@ router = APIRouterExtended(prefix="/users", tags=["Users"])
 
 
 def get_user_service() -> UserService:
+    """
+    Returns an instance of the UserService class.
+
+    Returns:
+        UserService: An instance of the UserService class.
+    """
+
     return UserService()
 
 
@@ -19,6 +26,20 @@ async def api_delete_me(
     service: UserService = Depends(get_user_service),
     current_user: User = Depends(current_active_user),
 ):
+    """
+    Deletes the current user.
+
+    Args:
+        service: The user service.
+        current_user: The current active user.
+
+    Returns:
+        Response: A response indicating the success or failure of the deletion.
+
+    Raises:
+        HTTPException: If the transaction is not found.
+    """
+
     result = await tm.transaction(service.delete_self, current_user)
 
     if result:
