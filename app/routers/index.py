628a224cf3c17a93e713dbdd7d94fcd255253816
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 17:22:54 +0100

    docstring

--- app/routers/index.py
+++ app/routers/index.py
@@ -1,14 +1,24 @@
 from fastapi import status
 from fastapi.responses import RedirectResponse
 
-from app.utils import PageRouter
 from app.routers.dashboard import router as dashboard_router
+from app.utils import PageRouter
 
 router = PageRouter(tags=["Index"])
 
 
 @router.get("/")
 async def page_index():
+    """
+    Redirects to the dashboard page.
+
+    Args:
+        None
+
+    Returns:
+        RedirectResponse: A redirect response to the dashboard page.
+    """
+
     return RedirectResponse(
         dashboard_router.routes[0].path,
         status_code=status.HTTP_302_FOUND,
