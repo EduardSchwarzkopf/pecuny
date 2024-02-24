00d73d7baed21a69db62c847731908ac825800df
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-24 19:26:44 +0100

    add new utility function for rendering

--- app/utils/account_utils.py
+++ app/utils/account_utils.py
@@ -1,6 +1,12 @@
 from typing import List
 
+from fastapi import Request
+
 from app import models
+from app.config import settings
+from app.services import accounts as service
+from app.utils.log_messages import ACCOUNT_USER_ID_MISMATCH
+from app.utils.template_utils import render_template
 
 
 def calculate_total_balance(account_list: List[models.Account]) -> float:
@@ -15,3 +21,20 @@ def calculate_total_balance(account_list: List[models.Account]) -> float:
     """
 
     return sum(account.balance for account in account_list)
+
+
+async def get_account_list_template(
+    user: models.User, template_name: str, request: Request
+):
+    account_list = await service.get_accounts(user)
+    total_balance = calculate_total_balance(account_list)
+
+    return render_template(
+        template_name,
+        request,
+        {
+            "account_list": account_list,
+            "max_allowed_accounts": settings.max_allowed_accounts,
+            "total_balance": total_balance,
+        },
+    )
