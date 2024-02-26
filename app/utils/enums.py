fcd25d7e3a517175d8ece243a61e31b743ec47a3
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-02-26 21:19:15 +0100

    add database operator enum

--- app/utils/enums.py
+++ app/utils/enums.py
@@ -19,3 +19,12 @@ class RequestMethod(Enum):
     POST = "post"
     PATCH = "patch"
     DELETE = "delete"
+
+
+class DatabaseFilterOperator(Enum):
+    EQUAL = "="
+    NOT_EQUAL = "<>"
+    LESS_THAN = "<"
+    LESS_THAN_OR_EQUAL = "<="
+    GREATER_THAN = ">"
+    GREATER_THAN_OR_EQUAL = ">="
