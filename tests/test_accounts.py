f47c71af915b9b1afd9c429706bcc898f38b801c
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-03-04 22:04:45 +0100

    refactor for single session

--- tests/test_accounts.py
+++ tests/test_accounts.py
@@ -1,7 +1,10 @@
 import pytest
 
+from app import models
+from app import repository as repo
 from app import schemas
-from app.utils.dataclasses_utils import ClientSessionWrapper
+from app.utils.enums import RequestMethod
+from tests.utils import make_http_request
 
 pytestmark = pytest.mark.anyio
 ENDPOINT = "/api/accounts/"
@@ -9,7 +12,7 @@ ENDPOINT = "/api/accounts/"
 # TODO: Add get account test
 
 
-async def test_create_account(client_session_wrapper: ClientSessionWrapper):
+async def test_create_account(test_user):
     """
     Tests the create account functionality.
 
@@ -21,11 +24,11 @@ async def test_create_account(client_session_wrapper: ClientSessionWrapper):
         None
     """
 
-    async with client_session_wrapper.session:
-        res = await client_session_wrapper.authorized_client.post(
-            ENDPOINT,
-            json={"label": "test_account", "description": "test", "balance": 500},
-        )
+    res = await make_http_request(
+        ENDPOINT,
+        json={"label": "test_account", "description": "test", "balance": 500},
+        as_user=test_user,
+    )
 
     assert res.status_code == 201
 
@@ -44,9 +47,7 @@ async def test_create_account(client_session_wrapper: ClientSessionWrapper):
         ("test", "test", "0,3"),
     ],
 )
-async def test_invalid_create_account(
-    client_session_wrapper: ClientSessionWrapper, label, description, balance
-):
+async def test_invalid_create_account(test_user, label, description, balance):
     """
     Tests the delete account functionality.
 
@@ -59,19 +60,18 @@ async def test_invalid_create_account(
         None
     """
 
-    async with client_session_wrapper.session:
-        res = await client_session_wrapper.authorized_client.post(
-            ENDPOINT,
-            json={"label": label, "description": description, "balance": balance},
-        )
+    res = await make_http_request(
+        ENDPOINT,
+        json={"label": label, "description": description, "balance": balance},
+        as_user=test_user,
+    )
 
     assert res.status_code == 422
 
 
-@pytest.mark.usefixtures("test_account")
-async def test_delete_account(client_session_wrapper: ClientSessionWrapper):
+async def test_delete_account(test_account):
     """
-    Tests the update account functionality.
+    Tests the delete account functionality.
 
     Args:
         session: The session fixture.
@@ -82,19 +82,21 @@ async def test_delete_account(client_session_wrapper: ClientSessionWrapper):
         None
     """
 
-    res = await client_session_wrapper.authorized_client.delete(f"{ENDPOINT}1")
+    user = await repo.get(models.User, test_account.user_id)
+    res = await make_http_request(
+        f"{ENDPOINT}{test_account.id}",
+        as_user=user,
+        method=RequestMethod.DELETE,
+    )
 
     assert res.status_code == 204
 
+    account = await repo.get(models.Account, test_account.id)
 
-@pytest.mark.parametrize(
-    "account_id, status_code",
-    [("2", 404), ("3", 404), ("4", 404), ("999999", 404)],
-)
-@pytest.mark.usefixtures("test_account")
-async def test_invalid_delete_account(
-    client_session_wrapper: ClientSessionWrapper, account_id, status_code
-):
+    assert account is None
+
+
+async def test_invalid_delete_account(test_user, test_accounts):
     """
     Tests the update account functionality.
 
@@ -107,48 +109,50 @@ async def test_invalid_delete_account(
         None
     """
 
-    res = await client_session_wrapper.authorized_client.delete(
-        f"{ENDPOINT}{account_id}"
-    )
+    status_code = 404
 
-    assert res.status_code == status_code
+    for account in test_accounts:
+
+        if account.user_id == test_user.id:
+            continue
+
+        res = await make_http_request(
+            f"{ENDPOINT}{account.id}", as_user=test_user, method=RequestMethod.DELETE
+        )
+
+        assert res.status_code == status_code
+
+        account_refresh = await repo.get(models.Account, account.id)
+
+        assert account_refresh == account
 
 
 @pytest.mark.parametrize(
     "values",
     [
-        (
-            {
-                "label": "My new Label",
-                "description": "very new description",
-                "balance": 1111.3,
-            }
-        ),
-        (
-            {
-                "label": "11113",
-                "description": "cool story bro '",
-                "balance": 2000,
-            }
-        ),
-        (
-            {
-                "label": "My new Label",
-                "description": "very new description",
-                "balance": -0.333333334,
-            }
-        ),
-        (
-            {
-                "label": "My new Label",
-                "description": "very new description",
-                "balance": -1000000.3,
-            }
-        ),
+        {
+            "label": "My new Label",
+            "description": "very new description",
+            "balance": 1111.3,
+        },
+        {
+            "label": "11113",
+            "description": "cool story bro '",
+            "balance": 2000,
+        },
+        {
+            "label": "My new Label",
+            "description": "very new description",
+            "balance": -0.333333334,
+        },
+        {
+            "label": "My new Label",
+            "description": "very new description",
+            "balance": -1000000.3,
+        },
     ],
 )
-@pytest.mark.usefixtures("test_account")
-async def test_update_account(client_session_wrapper: ClientSessionWrapper, values):
+async def test_update_account(test_account, values):
     """
     Tests the update account functionality.
 
@@ -161,18 +165,15 @@ async def test_update_account(client_session_wrapper: ClientSessionWrapper, valu
         None
     """
 
-    async with client_session_wrapper.session:
-        res = await client_session_wrapper.authorized_client.put(
-            f"{ENDPOINT}1", json=values
-        )
+    user = await repo.get(models.User, test_account.user_id)
+    response = await make_http_request(
+        f"{ENDPOINT}{test_account.id}", json=values, as_user=user
+    )
 
-    assert res.status_code == 200
-    account = schemas.AccountData(**res.json())
+    assert response.status_code == 200
+    account = schemas.AccountData(**response.json())
 
     for key, value in values.items():
-        if key == "id":
-            continue
-
         account_val = getattr(account, key)
         print(f"key: {key} | value: {value} | account_val: {account_val}")
         if isinstance(value, str):
