edd6b2d07bab5386e7875c9ae1ebafb385e14a12
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-03-11 20:25:26 +0100

    remove encoder

--- app/main.py
+++ app/main.py
@@ -3,7 +3,6 @@ from contextlib import asynccontextmanager
 
 import arel
 from fastapi import FastAPI, Request, status
-from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import JSONResponse
