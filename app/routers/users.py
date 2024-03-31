663dda8114be70eac68b5068fd50177dc6690c17
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-03-31 15:16:28 +0200

    add resubale settings response

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,9 +1,10 @@
 from fastapi import Depends, Request
-from fastapi.responses import RedirectResponse
+from fastapi.responses import HTMLResponse
 from starlette_wtf import csrf_protect
 
 from app import schemas
 from app.auth_manager import current_active_user
+from app.authentication.dependencies import get_user_service
 from app.models import User
 from app.services.users import UserService
 from app.utils import PageRouter
@@ -11,6 +12,16 @@ from app.utils.enums import FeedbackType
 from app.utils.template_utils import render_template, set_feedback
 
 router = PageRouter(tags=["Users"], prefix="/users")
+
+
+def get_settings_response(
+    request: Request, form: schemas.UpdateUserForm
+) -> HTMLResponse:
+    return render_template(
+        "pages/user/page_user_settings.html",
+        request,
+        {"form": form, "action_url": router.url_path_for("page_user_settings")},
+    )
 
 
 @csrf_protect
@@ -32,11 +43,10 @@ async def page_user_settings_form(
 
     form = schemas.UpdateUserForm(request, data=user.__dict__)
 
-    return render_template(
-        "pages/user/page_user_settings.html",
-        request,
-        {"form": form, "action_url": router.url_path_for("page_user_settings")},
-    )
+    if not user.is_verified:
+        set_feedback(request, FeedbackType.ERROR, "You need to verify your email first")
+
+    return get_settings_response(request, form)
 
 
 @csrf_protect
@@ -44,6 +54,7 @@ async def page_user_settings_form(
 async def page_user_settings(
     request: Request,
     user: User = Depends(current_active_user),
+    service: UserService = Depends(get_user_service),
 ):
     """
     Handles the user settings form submission.
@@ -68,10 +79,8 @@ async def page_user_settings(
             {"form": form, "action_url": router.url_path_for("page_user_settings")},
         )
 
-    user.email = form.email.data
-    user.displayname = form.displayname.data
+    user_data = schemas.UserUpdate(email=user.email, displayname=user.displayname)
 
-    user_service = UserService()
-    await user_service.update_user(user)
+    await service.update_user(user, user_data)
 
-    return RedirectResponse("/", 302)
+    return get_settings_response(request, form)
