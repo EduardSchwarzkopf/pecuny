b45b451444a218d8911e5d316278cd5b24e84257
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-04-01 15:03:15 +0200

    add register view test

--- tests/view/test_view_auth.py
+++ tests/view/test_view_auth.py
@@ -1,13 +1,42 @@
+from bs4 import BeautifulSoup
 from httpx import Cookies
-from starlette.status import HTTP_401_UNAUTHORIZED
+from starlette.status import HTTP_200_OK, HTTP_401_UNAUTHORIZED
 
 from app import models
 from app.config import settings
 from app.utils.enums import RequestMethod
 from tests.utils import make_http_request
 
-# async def test_create_user():
-#     pass
+
+async def test_view_register():
+
+    url = "/register"
+    res = await make_http_request(url=url, method=RequestMethod.GET)
+
+    assert res.status_code == HTTP_200_OK
+
+    text = res.text
+    soup = BeautifulSoup(text)
+
+    form = soup.find("form")
+    assert form["method"] == "POST"
+    assert url in form["action"]
+
+    csrf_field = form.find("input", id="csrf_token")
+    csrf_attr = csrf_field.attrs
+    assert csrf_attr["type"] == "hidden"
+    assert csrf_attr["name"] == "csrf_token"
+    assert bool(csrf_attr["value"].strip())
+
+    email_field = form.find("input", id="email")
+
+    assert email_field["onkeyup"] == "hideError(this)"
+    assert int(email_field["maxlength"]) == 320
+
+    password_field = form.find("input", id="password")
+
+    assert password_field["onkeyup"] == "hideError(this)"
+    assert password_field["type"] == "password"
 
 
 # async def test_invalid_create_user():
