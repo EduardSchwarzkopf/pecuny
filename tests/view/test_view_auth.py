546748a110f7cef0c7dae72e33d695cc73209e40
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-04-01 16:19:30 +0200

    apply DRY principle

--- tests/view/test_view_auth.py
+++ tests/view/test_view_auth.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
+from httpx import QueryParams
 from starlette.status import HTTP_200_OK
 
 from app.utils.enums import RequestMethod
@@ -6,117 +7,69 @@ from tests.form_utils import base_form_test
 from tests.utils import make_http_request
 
 
-async def test_view_register():
-
-    url = "/register"
-    res = await make_http_request(url=url, method=RequestMethod.GET)
-
+async def view_base_test(
+    url: str,
+    expected_inputs: int,
+    action_url: str = None,
+    email_id: str = None,
+    password_id: str = None,
+    username_id: str = None,
+    params: QueryParams = None,
+) -> Tag:
+    res = await make_http_request(url=url, method=RequestMethod.GET, params=params)
     assert res.status_code == HTTP_200_OK
 
-    text = res.text
-    soup = BeautifulSoup(text)
+    soup = BeautifulSoup(res.text)
 
     form = soup.find("form")
-    base_form_test(form, url)
 
-    assert len(form.find_all("input")) == 3
+    if action_url is None:
+        action_url = url
 
-    email_field = form.find("input", id="email")
+    base_form_test(form, action_url)
 
-    assert email_field["onkeyup"] == "hideError(this)"
-    assert int(email_field["maxlength"]) == 320
+    assert len(form.find_all("input")) == expected_inputs
 
-    password_field = form.find("input", id="password")
+    if email_id:
+        email_field = form.find("input", id=email_id)
+        assert email_field["onkeyup"] == "hideError(this)"
 
-    assert password_field["onkeyup"] == "hideError(this)"
-    assert password_field["type"] == "password"
+    if password_id:
+        password_field = form.find("input", id=password_id)
+        assert password_field["onkeyup"] == "hideError(this)"
+        assert password_field["type"] == "password"
+
+    if username_id:
+        username_field = form.find("input", id=username_id)
+        assert username_field["onkeyup"] == "hideError(this)"
+
+    return form
+
+
+async def test_view_register():
+    await view_base_test("/register", 3, email_id="email", password_id="password")
 
 
 async def test_view_login():
-
-    url = "/login"
-    res = await make_http_request(url=url, method=RequestMethod.GET)
-
-    assert res.status_code == HTTP_200_OK
-
-    text = res.text
-    soup = BeautifulSoup(text)
-
-    form = soup.find("form")
-    base_form_test(form, url)
-
-    assert len(form.find_all("input")) == 3
-
-    username_field = form.find("input", id="username")
-
-    assert username_field["onkeyup"] == "hideError(this)"
-
-    password_field = form.find("input", id="password")
-
-    assert password_field["onkeyup"] == "hideError(this)"
-    assert password_field["type"] == "password"
+    await view_base_test("/login", 3, email_id="username", password_id="password")
 
 
 async def test_view_forgot_password():
-
-    url = "/forgot-password"
-    res = await make_http_request(url=url, method=RequestMethod.GET)
-
-    assert res.status_code == HTTP_200_OK
-
-    text = res.text
-    soup = BeautifulSoup(text)
-
-    form = soup.find("form")
-    base_form_test(form, url)
-
-    assert len(form.find_all("input")) == 2
-
-    email_field = form.find("input", id="email")
-
-    assert email_field["onkeyup"] == "hideError(this)"
+    await view_base_test("/forgot-password", 2, email_id="email")
 
 
-async def test_view_forgot_password():
-
-    res = await make_http_request(url="/get-verify-token", method=RequestMethod.GET)
-
-    assert res.status_code == HTTP_200_OK
-
-    text = res.text
-    soup = BeautifulSoup(text)
-
-    form = soup.find("form")
-    base_form_test(form, "send-verify-token")
-
-    assert len(form.find_all("input")) == 2
-
-    email_field = form.find("input", id="email")
-
-    assert email_field["onkeyup"] == "hideError(this)"
+async def test_view_get_verify_token():
+    await view_base_test(
+        "/get-verify-token", 2, action_url="send-verify-token", email_id="email"
+    )
 
 
 async def test_view_reset_password():
-
-    url = "/reset-password"
     token_value = "test"
-    res = await make_http_request(
-        url=url, method=RequestMethod.GET, params={"token": token_value}
+
+    form = await view_base_test(
+        "/reset-password", 3, password_id="password", params={"token": token_value}
     )
-
-    assert res.status_code == HTTP_200_OK
-
-    text = res.text
-    soup = BeautifulSoup(text)
-
-    form = soup.find("form")
-    base_form_test(form, url)
-
-    assert len(form.find_all("input")) == 3
-
-    password_field = form.find("input", id="password")
-
-    assert password_field["type"] == "password"
 
     token_field = form.find("input", id="token")
 
