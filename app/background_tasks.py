4070b8bf51c4cfa4f7488ba4f0ced76d1513a544
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-04-21 07:45:34 +0200

    update function to handle session and like operator

--- app/background_tasks.py
+++ app/background_tasks.py
@@ -1,4 +1,7 @@
-from typing import List
+from csv import DictReader
+from decimal import InvalidOperation
+
+from pydantic import ValidationError
 
 from app import models, schemas, transaction_manager
 from app.config import settings
@@ -7,13 +10,16 @@ from app.logger import get_logger
 from app.repository import Repository
 from app.services.email import send_transaction_import_report
 from app.services.transactions import TransactionService
+from app.utils.dataclasses_utils import FailedImportedTransaction
+from app.utils.enums import DatabaseFilterOperator
 
 logger = get_logger(__name__)
 
 
-async def import_transactions(
+async def import_transactions_from_csv(
     user: models.User,
-    transaction_data_list: List[schemas.TransactionInformationCreate],
+    account_id: int,
+    reader: DictReader,
 ) -> None:
     """
     Imports a list of transactions for a user.
@@ -26,23 +32,83 @@ async def import_transactions(
         None
     """
 
-    session = await db.get_session()
-    repo = Repository(session)
-    transaction_service = TransactionService(repo)
-    failed_transaction_list = []
+    async with await db.get_session() as session:
 
-    for transaction_data in transaction_data_list:
-        transaction = await transaction_manager.transaction(
-            transaction_service.create_transaction,
-            user,
-            transaction_data,
-            session=session,
-        )
+        repo = Repository(session)
+        service = TransactionService(repo)
+        failed_transaction_list: list[FailedImportedTransaction] = []
 
-        if transaction is None:
-            failed_transaction_list.append(transaction_data)
+        for row in reader:
+            failed_transaction = FailedImportedTransaction(**row)
+            row_section = row.get("section")
+            section_list = await repo.filter_by(
+                models.TransactionSection,
+                models.TransactionSection.label,
+                row_section,
+                DatabaseFilterOperator.LIKE,
+            )
+
+            if not section_list:
+                failed_transaction.reason = f"Section {row_section} not found"
+                failed_transaction_list.append(failed_transaction)
+                continue
+
+            section = section_list[0]
+
+            conditions = [
+                (
+                    models.TransactionCategory.label,
+                    row.get("category"),
+                    DatabaseFilterOperator.LIKE,
+                ),
+                (
+                    models.TransactionCategory.section_id,
+                    section.id,
+                    DatabaseFilterOperator.EQUAL,
+                ),
+            ]
+
+            category_list = await repo.filter_by_multiple(
+                models.TransactionCategory, conditions
+            )
+            category = category_list[0] if category_list else None
+
+            if category is None:
+                failed_transaction.reason = f"Category {row['category']} not found"
+                failed_transaction_list.append(failed_transaction)
+                continue
+
+            try:
+
+                transaction_data = schemas.TransactionInformationCreate(
+                    account_id=account_id, category_id=category.id, **row
+                )
+            except ValidationError as e:
+                first_error = e.errors()[0]
+                custom_error_message = f"{first_error['loc'][0]}: {first_error['msg']}"
+                failed_transaction.reason = custom_error_message
+                failed_transaction_list.append(failed_transaction)
+                continue
+            except InvalidOperation as e:
+                msg = (
+                    f"Invalid value on line {reader.line_num} on value {row['amount']}"
+                )
+                failed_transaction.reason = msg
+                failed_transaction_list.append(failed_transaction)
+                continue
+
+            transaction = await transaction_manager.transaction(
+                service.create_transaction,
+                user,
+                transaction_data,
+                session=session,
+            )
+
+            if transaction is None:
+                failed_transaction.reason = "Failed to create transaction"
+                failed_transaction_list.append(failed_transaction)
 
     if not settings.is_testing_environment:
         await send_transaction_import_report(
-            user, len(transaction_data_list), failed_transaction_list
+            user, reader.line_num - 1, failed_transaction_list
         )
