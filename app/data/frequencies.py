6a5e98b6571cca643b68cfd3470d7a50bf1490f4
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-17 06:42:09 +0200

    add enums

--- app/data/frequencies.py
+++ app/data/frequencies.py
@@ -1,3 +1,6 @@
+from app.utils.enums import Frequency as frq
+
+
 def get_frequency_list():
     """
     Returns a list of all available frequencies.
@@ -6,6 +9,11 @@ def get_frequency_list():
         Each dictionary contains the id and label of a frequency.
     """
 
-    frequencies = ["once", "daily", "weekly", "monthly", "yearly"]
-
-    return [{"id": i + 1, "label": frequencies[i]} for i in range(len(frequencies))]
+    frequencies = {
+        frq.ONCE.name: frq.ONCE.value,
+        frq.DAILY.name: frq.DAILY.value,
+        frq.WEEKLY.name: frq.WEEKLY.value,
+        frq.MONTHLY.name: frq.MONTHLY.value,
+        frq.YEARLY.name: frq.YEARLY.value,
+    }
+    return [{"id": value, "label": key} for key, value in frequencies.items()]
