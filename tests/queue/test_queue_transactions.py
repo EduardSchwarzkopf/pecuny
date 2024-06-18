66fe9350a9cf951a70853f7db80a4b5e4b84a917
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-18 05:26:55 +0200

    add queue tests

---
+++ tests/queue/test_queue_transactions.py
@@ -0,0 +1,19 @@
+# use fixture for scheduled transactions
+def test_create_daily_transaction(create_scheduled_transactions):
+    assert False
+
+
+def test_create_weekly_transaction():
+    assert False
+
+
+def test_create_yearly_transaction():
+    assert False
+
+
+def test_scheduled_transaction_date_end():
+    assert False
+
+
+def test_scheduled_transaction_date_start():
+    assert False
