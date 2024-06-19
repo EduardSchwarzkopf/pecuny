8f5bd32b44d8770c878359034eab35040d97864b
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-06-19 18:47:26 +0200

    remove sanity check

--- tests/queue/test_queue_transactions.py
+++ tests/queue/test_queue_transactions.py
@@ -1,11 +1,9 @@
-# use fixture for scheduled transactions
 import time
 from typing import List
 
 from app import models
 from app.repository import Repository
 from app.tasks import create_transactions_for_batch
-from app.utils.enums import DatabaseFilterOperator
 
 
 async def test_create_daily_transaction(
@@ -26,17 +24,6 @@ async def test_create_daily_transaction(
     time.sleep(1)
     repository.session.expire_all()
 
-    created_transaction_list = await repository.filter_by_multiple(
-        models.Transaction,
-        [
-            (
-                models.Transaction.scheduled_transaction_id,
-                None,
-                DatabaseFilterOperator.IS_NOT,
-            ),
-            (models.Transaction.account_id, account_id, DatabaseFilterOperator.EQUAL),
-        ],
-    )
     account = await repository.get(models.Account, account_id)
     new_balance = account.balance
 
