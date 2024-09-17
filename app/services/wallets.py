e5b6ddc17a5633ff30780d6b2c0bca79ad7eaf0f
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-17 19:05:23 +0200

    refactor to use new session transaction manager

--- app/services/wallets.py
+++ app/services/wallets.py
@@ -4,6 +4,7 @@ from app import models, schemas
 from app.config import settings
 from app.logger import get_logger
 from app.services.base import BaseService
+from app.utils.exceptions import AccessDeniedException, WalletNotFoundException
 
 logger = get_logger(__name__)
 
@@ -31,109 +32,55 @@ class WalletService(BaseService):
     async def get_wallet(
         self, current_user: models.User, wallet_id: int
     ) -> Optional[models.Wallet]:
-        """
-        Retrieves an wallet by ID.
 
-        Args:
-            current_user: The current active user.
-            wallet_id: The ID of the wallet to retrieve.
-
-        Returns:
-            Wallet: The retrieved wallet object.
-        """
-
-        logger.info("Getting wallet %s for user: %s", wallet_id, current_user.id)
         wallet = await self.repository.get(models.Wallet, wallet_id)
 
         if wallet is None:
-            return None
+            raise WalletNotFoundException(wallet_id)
 
         if self.has_user_access_to_wallet(current_user, wallet):
             logger.info("Found wallet %s for user: %s", wallet_id, current_user.id)
             return wallet
 
-        return None
+        raise AccessDeniedException(current_user.id, wallet_id)
 
     async def create_wallet(
         self, user: models.User, wallet: schemas.Wallet
     ) -> models.Wallet:
-        """
-        Creates a new wallet.
 
-        Args:
-            user: The user object.
-            wallet: The wallet data.
-
-        Returns:
-            Wallet: The created wallet object.
-        """
-
-        logger.info("Creating new wallet for user: %s", user.id)
         db_wallet = models.Wallet(user=user, **wallet.model_dump())
         await self.repository.save(db_wallet)
-        logger.info("Wallet created for user: %s", user.id)
         return db_wallet
 
     async def update_wallet(
         self, current_user: models.User, wallet_id, wallet: schemas.WalletData
-    ) -> Optional[models.Wallet]:
-        """
-        Updates an wallet.
+    ) -> models.Wallet:
 
-        Args:
-            current_user: The current active user.
-            wallet_id: The ID of the wallet to update.
-            wallet: The updated wallet data.
-
-        Returns:
-            Wallet: The updated wallet information.
-        """
-
-        logger.info("Updating wallet %s for user: %s", wallet_id, current_user.id)
         db_wallet = await self.repository.get(models.Wallet, wallet_id)
 
         if db_wallet is None:
-            return None
+            raise WalletNotFoundException(wallet_id)
 
-        if db_wallet.user_id == current_user.id:
+        if self.has_user_access_to_wallet(current_user, db_wallet):
             await self.repository.update(
                 models.Wallet, db_wallet.id, **wallet.model_dump()
             )
-            logger.info("Wallet %s updated for user:  %s", wallet, current_user.id)
             return db_wallet
-        logger.warning(
-            "Wallet %s not found or does not belong to user: %s",
-            wallet,
-            current_user.id,
-        )
-        return None
 
-    async def delete_wallet(
-        self, current_user: models.User, wallet_id: int
-    ) -> Optional[bool]:
-        """
-        Deletes an wallet.
+        raise AccessDeniedException(current_user.id, wallet_id)
 
-        Args:
-            current_user: The current active user.
-            wallet_id: The ID of the wallet to delete.
+    async def delete_wallet(self, current_user: models.User, wallet_id: int) -> True:
 
-        Returns:
-            bool: True if the wallet is successfully deleted, False otherwise.
-        """
-
-        logger.info("Deleting wallet %s for user: %s", wallet_id, current_user.id)
         wallet = await self.repository.get(models.Wallet, wallet_id)
-        if wallet and wallet.user_id == current_user.id:
+
+        if wallet is None:
+            raise WalletNotFoundException(wallet_id)
+
+        if self.has_user_access_to_wallet(current_user, wallet):
             await self.repository.delete(wallet)
-            logger.info("Wallet %s deleted for user: %s", wallet_id, current_user.id)
             return True
-        logger.warning(
-            "Wallet %s not found or does not belong to user: %s",
-            wallet_id,
-            current_user.id,
-        )
-        return None
+
+        raise AccessDeniedException(current_user.id, wallet_id)
 
     async def check_max_wallets(self, user: models.User) -> bool:
         """
@@ -146,7 +93,6 @@ class WalletService(BaseService):
             bool: True if the maximum number of wallets has been reached, False otherwise.
         """
 
-        logger.info("Checking if maximum wallets reached for user: %s", user.id)
         wallet_list = await self.get_wallets(user)
 
         if wallet_list is None:
