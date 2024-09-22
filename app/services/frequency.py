482a0d711878d13b82ee3dfe0073424005022f83
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-22 09:25:58 +0200

    update to provide logger

--- app/services/frequency.py
+++ app/services/frequency.py
@@ -2,12 +2,17 @@ from typing import Optional
 
 from app import models
 from app.logger import get_logger
+from app.repository import Repository
 from app.services.base import BaseService
 
 logger = get_logger(__name__)
 
 
 class FrequencyService(BaseService):
+
+    def __init__(self, repository: Optional[Repository] = None):
+        self.logger = logger
+        super().__init__(logger, repository)
 
     async def get_frequency_list(
         self,
