3159799851916749d6e4552c897976b6d05f6972
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-22 09:23:22 +0200

    handle message in exception

--- app/exceptions/user_service_exceptions.py
+++ app/exceptions/user_service_exceptions.py
@@ -1,10 +1,14 @@
+from pydantic import EmailStr
+
 from app.exceptions.base_service_exception import BaseServiceException
 
 
 class UserAlreadyExistsException(BaseServiceException):
-    def __init__(self):
-        super().__init__("User with given email already exists.")
+    def __init__(self, email: EmailStr):
+        super().__init__(f"User with email {email} already exists")
 
 
 class UserNotFoundException(BaseServiceException):
-    pass
+
+    def __init__(self, email: EmailStr):
+        super().__init__(f"No user found with email {email}")
