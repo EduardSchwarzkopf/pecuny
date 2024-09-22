af228d9ee1e36838ac79d0493e6e1b28e9410823
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-22 09:25:43 +0200

    introduce log_and_raise_exception function

--- app/services/base.py
+++ app/services/base.py
@@ -1,14 +1,35 @@
 from abc import ABC
+from logging import Logger
 from typing import Optional
 
+from app.exceptions.base_service_exception import BaseServiceException
 from app.repository import Repository
 
 
 class BaseService(ABC):
-    def __init__(self, repository: Optional[Repository] = None):
+    def __init__(self, logger: Logger, repository: Optional[Repository] = None):
         if repository is None:
             repository = Repository()
+
         self.repository = repository
+        self.logger = logger
+
+    def log_and_raise_exception(self, exception: BaseServiceException):
+        """
+        Logs the given exception using the logger and then raises the exception.
+
+        Args:
+            exception (BaseServiceException): The exception to be logged and raised.
+
+        Raises:
+            BaseServiceException: The exception passed as an argument.
+
+        Returns:
+            None
+        """
+
+        self.logger.info(exception)
+        raise exception
 
     @classmethod
     def get_instance(cls):
