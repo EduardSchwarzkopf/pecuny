8f078abcbb175759cdd44c18aae1cbb7ad3a013a
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-23 05:54:49 +0200

    remove parameter for 404 exception

--- app/exceptions/http_exceptions.py
+++ app/exceptions/http_exceptions.py
@@ -18,7 +18,7 @@ class HTTPForbiddenException(HTTPException):
 
 
 class HTTPNotFoundException(HTTPException):
-    def __init__(self, entity_name: str, entity_id: int):
+    def __init__(self):
         super().__init__(
             status_code=status.HTTP_404_NOT_FOUND,
             detail="This is not the resource you are looking for.",
