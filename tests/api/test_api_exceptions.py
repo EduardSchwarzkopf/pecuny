564b1436266b5daea68acdbea3e06813ebe7102f
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-26 20:39:33 +0200

    test for exceptions added

---
+++ tests/api/test_api_exceptions.py
@@ -0,0 +1,34 @@
+def test_api_not_found_error():
+    raise NotImplementedError()
+
+
+def test_api_internal_server_error():
+    raise NotImplementedError()
+
+
+def test_api_unauthorized_error():
+    raise NotImplementedError()
+
+
+def test_api_request_validation_error():
+    raise NotImplementedError()
+
+
+def test_api_forbidden_error():
+    raise NotImplementedError()
+
+
+def test_api_timeout_error():
+    raise NotImplementedError()
+
+
+def test_api_connection_error():
+    raise NotImplementedError()
+
+
+def test_create_invalid_data():
+    raise NotImplementedError()
+
+
+def test_api_method_not_allowed():
+    raise NotImplementedError()
---
+++ tests/view/test_view_exception.py
@@ -0,0 +1,34 @@
+def test_view_not_found_error():
+    raise NotImplementedError()
+
+
+def test_view_internal_server_error():
+    raise NotImplementedError()
+
+
+def test_view_unauthorized_error():
+    raise NotImplementedError()
+
+
+def test_view_request_validation_error():
+    raise NotImplementedError()
+
+
+def test_view_forbidden_error():
+    raise NotImplementedError()
+
+
+def test_view_timeout_error():
+    raise NotImplementedError()
+
+
+def test_view_connection_error():
+    raise NotImplementedError()
+
+
+def test_create_invalid_data():
+    raise NotImplementedError()
+
+
+def test_view_method_not_allowed():
+    raise NotImplementedError()
