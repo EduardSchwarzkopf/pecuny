4bf8d252167c7cde6cfecfb5104d4eb090a7b18d
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-26 19:20:18 +0200

    add WalletReachedException

--- app/exceptions/wallet_service_exceptions.py
+++ app/exceptions/wallet_service_exceptions.py
@@ -7,6 +7,7 @@ from app.exceptions.base_service_exception import (
 
 class WalletAccessDeniedException(EntityAccessDeniedException):
     def __init__(self, user: models.User, wallet: models.Wallet):
-        self.wallet = wallet
-        self.user = user
-        super().__init__(self.user, self.wallet)
+
+class WalletLimitReachedException(BaseServiceException):
+    def __init__(self, user: models.User):
+        super().__init__(f"User with {user.id} has reached the limit of wallets.")
