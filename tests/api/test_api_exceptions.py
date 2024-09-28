787bcd4f029cfce4491b0ce72e72161422237704
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 19:04:51 +0200

    add api tests for error handling

--- tests/api/test_api_exceptions.py
+++ tests/api/test_api_exceptions.py
@@ -1,34 +1,61 @@
-def test_api_not_found_error():
-    raise NotImplementedError()
+from typing import Optional
+
+from starlette.status import (
+    HTTP_401_UNAUTHORIZED,
+    HTTP_403_FORBIDDEN,
+    HTTP_404_NOT_FOUND,
+    HTTP_405_METHOD_NOT_ALLOWED,
+    HTTP_422_UNPROCESSABLE_ENTITY,
+    HTTP_500_INTERNAL_SERVER_ERROR,
+)
+
+from app.utils.enums import RequestMethod
+from tests.utils import make_http_request
 
 
-def test_api_internal_server_error():
-    raise NotImplementedError()
+async def assert_error_response(
+    status_code: int, excpected_status_code: Optional[int] = None
+):
+    res = await make_http_request(
+        f"/api/errors/raise/{status_code}", method=RequestMethod.GET
+    )
+
+    if excpected_status_code:
+        assert res.status_code == excpected_status_code
+    else:
+        assert res.status_code == status_code
+
+    json_response = res.json()
+
+    assert "detail" in json_response
 
 
-def test_api_unauthorized_error():
-    raise NotImplementedError()
+async def test_api_not_found_error():
+    res = await make_http_request(
+        "/api/not-found", follow_redirects=True, method=RequestMethod.GET
+    )
+
+    assert res.status_code == HTTP_404_NOT_FOUND
+    json_response = res.json()
+
+    assert "detail" in json_response
 
 
-def test_api_request_validation_error():
-    raise NotImplementedError()
+async def test_api_internal_server_error():
+    await assert_error_response(HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-def test_api_forbidden_error():
-    raise NotImplementedError()
+async def test_api_unauthorized_error():
+    await assert_error_response(HTTP_401_UNAUTHORIZED)
 
 
-def test_api_timeout_error():
-    raise NotImplementedError()
+async def test_api_request_validation_error():
+    await assert_error_response(HTTP_422_UNPROCESSABLE_ENTITY)
 
 
-def test_api_connection_error():
-    raise NotImplementedError()
+async def test_api_forbidden_error():
+    await assert_error_response(HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND)
 
 
-def test_create_invalid_data():
-    raise NotImplementedError()
-
-
-def test_api_method_not_allowed():
-    raise NotImplementedError()
+async def test_api_method_not_allowed():
+    await assert_error_response(HTTP_405_METHOD_NOT_ALLOWED)
