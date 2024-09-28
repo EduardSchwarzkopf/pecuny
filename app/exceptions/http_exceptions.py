06fd1096ff84e1cea62cddd0a7a141b4b77a4f79
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 19:10:15 +0200

    make bad request standard details

--- app/exceptions/http_exceptions.py
+++ app/exceptions/http_exceptions.py
@@ -52,11 +52,8 @@ class HTTPNotFoundException(HTTPException):
 
 
 class HTTPBadRequestException(HTTPException):
-    def __init__(self, detail: str):
-        super().__init__(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=detail,
-        )
+    def __init__(self):
+        super().__init__(status_code=status.HTTP_400_BAD_REQUEST, detail="Bad Request.")
 
 
 class HTTPInternalServerException(HTTPException):
