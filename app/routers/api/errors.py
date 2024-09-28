d219cb320ade3d91e3485d7778b0288a5fe31d61
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 19:04:39 +0200

    make use of central raise http error function

--- app/routers/api/errors.py
+++ app/routers/api/errors.py
@@ -1,14 +1,15 @@
 from fastapi.responses import RedirectResponse
 
 from app.config import settings
+from app.exceptions.http_exceptions import raise_http_error
 from app.utils import APIRouterExtended
 
-router = APIRouterExtended(prefix="/error", tags=["Error"])
+router = APIRouterExtended(tags=["Errors"], prefix="/errors")
 
 
-@router.get("/raise-internal-error")
-async def api_raise_internal_error():
-    if settings.environment == "test":
-        raise Exception("This is a test exception for internal server error.")
+@router.get("/raise/{status_code}")
+async def raise_error(status_code: int):
+    if settings.environment != "test":
+        return RedirectResponse(f"/", status_code=302)
 
-    return RedirectResponse(f"/", status_code=302)
+    raise_http_error(status_code)
--- app/routers/errors.py
+++ app/routers/errors.py
@@ -1,16 +1,7 @@
-from fastapi import HTTPException
-from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse
 
 from app.config import settings
-from app.exceptions.http_exceptions import (
-    HTTPBadRequestException,
-    HTTPForbiddenException,
-    HTTPInternalServerException,
-    HTTPMethodNotAllowedException,
-    HTTPNotFoundException,
-    HTTPUnauthorizedException,
-)
+from app.exceptions.http_exceptions import raise_http_error
 from app.utils import PageRouter
 
 router = PageRouter(tags=["Error"], prefix="/errors")
@@ -21,25 +12,4 @@ async def raise_error(status_code: int):
     if settings.environment != "test":
         return RedirectResponse(f"/", status_code=302)
 
-    if status_code == 400:
-        raise HTTPBadRequestException()
-
-    if status_code == 401:
-        raise HTTPUnauthorizedException()
-
-    if status_code == 403:
-        raise HTTPForbiddenException()
-
-    if status_code == 404:
-        raise HTTPNotFoundException()
-
-    if status_code == 405:
-        raise HTTPMethodNotAllowedException()
-
-    if status_code == 422:
-        raise RequestValidationError([])
-
-    if status_code == 500:
-        raise HTTPInternalServerException()
-
-    raise HTTPException(status_code=status_code, detail="Custom error")
+    raise_http_error(status_code)
