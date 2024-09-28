02001abb054e56fb73c7d06bbcc8b9ee1fe960be
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 16:56:05 +0200

    fix path

--- tests/view/test_view_exception.py
+++ tests/view/test_view_exception.py
@@ -19,9 +19,7 @@ async def test_view_not_found_error():
 
 
 async def test_view_internal_server_error():
-    res = await make_http_request(
-        "/error/raise-test-error/500", method=RequestMethod.GET
-    )
+    res = await make_http_request("/error/raise/500", method=RequestMethod.GET)
 
     assert res.status_code == HTTP_500_INTERNAL_SERVER_ERROR
 
