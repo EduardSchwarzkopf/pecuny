0ac8342b2db41591bd4103b97a923d9d9f800076
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 17:42:02 +0200

    add Method not allowed Exception

--- app/exceptions/http_exceptions.py
+++ app/exceptions/http_exceptions.py
@@ -39,3 +39,11 @@ class HTTPInternalServerException(HTTPException):
             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
             detail="Something went wrong.",
         )
+
+
+class HTTPMethodNotAllowedException(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+            detail="This method is not allowed.",
+        )
