3785c2c1d97aec08e76f919db2e5bd7b38df32a0
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-28 16:32:44 +0200

    make endpoint more generic

--- app/routers/errors.py
+++ app/routers/errors.py
@@ -1,15 +1,41 @@
+from fastapi import HTTPException, status
+from fastapi.exceptions import RequestValidationError
 from fastapi.responses import RedirectResponse
 
 from app.config import settings
-from app.exceptions.http_exceptions import HTTPInternalServerException
+from app.exceptions.http_exceptions import (
+    HTTPBadRequestException,
+    HTTPForbiddenException,
+    HTTPInternalServerException,
+    HTTPNotFoundException,
+    HTTPUnauthorizedException,
+)
 from app.utils import PageRouter
 
 router = PageRouter(tags=["Error"], prefix="/error")
 
 
-@router.get("/raise-internal-error")
-async def raise_internal_error():
-    if settings.environment == "test":
+@router.get("/raise-test-error/{status_code}")
+async def raise_error(status_code: int):
+    if settings.environment != "test":
+        return RedirectResponse(f"/", status_code=302)
+
+    if status_code == 422:
+        raise RequestValidationError()
+
+    if status_code == 400:
+        raise HTTPBadRequestException()
+
+    if status_code == 500:
         raise HTTPInternalServerException()
 
-    return RedirectResponse(f"/", status_code=302)
+    if status_code == 404:
+        raise HTTPNotFoundException()
+
+    if status_code == 403:
+        raise HTTPForbiddenException()
+
+    if status_code == 401:
+        raise HTTPUnauthorizedException()
+
+    raise HTTPException(status_code=status_code, detail="Custom error")
