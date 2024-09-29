c266c4ae2fc81534c005edfb116f70fde5dde0a8
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-29 10:13:54 +0200

    add docstrings

--- tests/api/test_api_exceptions.py
+++ tests/api/test_api_exceptions.py
@@ -16,6 +16,14 @@ from tests.utils import make_http_request
 async def assert_error_response(
     status_code: int, expected_status_code: Optional[int] = None
 ):
+    """
+    Asserts the error response received from an HTTP request.
+
+    Args:
+        status_code: The expected status code of the response.
+        expected_status_code: The optional expected status code to compare with the response status code.
+    """
+
     res = await make_http_request(
         f"/api/errors/raise/{status_code}", method=RequestMethod.GET
     )
@@ -31,6 +39,10 @@ async def assert_error_response(
 
 
 async def test_api_not_found_error():
+    """
+    Sends an HTTP GET request to the "/api/not-found" endpoint with follow redirects enabled.
+    """
+
     res = await make_http_request(
         "/api/not-found", follow_redirects=True, method=RequestMethod.GET
     )
@@ -42,20 +54,35 @@ async def test_api_not_found_error():
 
 
 async def test_api_internal_server_error():
+    """
+    Tests the API behavior for an internal server error response.
+    """
     await assert_error_response(HTTP_500_INTERNAL_SERVER_ERROR)
 
 
 async def test_api_unauthorized_error():
+    """
+    Tests the API behavior for an unauthorized error response.
+    """
     await assert_error_response(HTTP_401_UNAUTHORIZED)
 
 
 async def test_api_request_validation_error():
+    """
+    Tests the API behavior for a request validation error response.
+    """
     await assert_error_response(HTTP_422_UNPROCESSABLE_ENTITY)
 
 
 async def test_api_forbidden_error():
+    """
+    Tests the API behavior for a forbidden error response.
+    """
     await assert_error_response(HTTP_403_FORBIDDEN, HTTP_404_NOT_FOUND)
 
 
 async def test_api_method_not_allowed():
+    """
+    Tests the API behavior for a method not allowed error response.
+    """
     await assert_error_response(HTTP_405_METHOD_NOT_ALLOWED)
