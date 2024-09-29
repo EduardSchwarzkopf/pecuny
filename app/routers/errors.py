4050480b26044a8c7e08a38d598f7079ccf65fd5
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-29 10:03:47 +0200

    add docstring

--- app/routers/errors.py
+++ app/routers/errors.py
@@ -9,6 +9,17 @@ router = PageRouter(tags=["Error"], prefix="/errors")
 
 @router.get("/raise/{status_code}")
 async def raise_error(status_code: int):
+    """
+    Handles the endpoint to raise an error based on the provided status code.
+
+    Args:
+        status_code: The HTTP status code to determine the type of error to raise.
+
+    Returns:
+        RedirectResponse: If the environment is not "test".
+        None: If the environment is "test" and an error is raised.
+    """
+
     if settings.environment != "test":
         return RedirectResponse("/", status_code=302)
 
