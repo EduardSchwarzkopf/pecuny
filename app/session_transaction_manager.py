8f9f19e418fceeee45a03d5372fe14229ae31207
Eduard Schwarzkopf <48969167+EduardSchwarzkopf@users.noreply.github.com>
2024-09-29 10:13:58 +0200

    add docstrings

--- app/session_transaction_manager.py
+++ app/session_transaction_manager.py
@@ -23,13 +23,23 @@ class SessionTransactionManager:
         if exc_type:
             await self.session.rollback()
             # TODO: Add condition to log only unhandled erros?
-            logger.warning(f"Transaction rolled back due to: {exc_val}")
+            logger.warning("Transaction rolled back due to: %s", exc_val)
         else:
             await self.session.commit()
             for entity in self.entities_to_refresh:
                 await self.session.refresh(entity)
 
     def add_for_refresh(self, entity):
+        """
+        Adds an entity to the list of entities to be refreshed.
+
+        Args:
+            entity: The entity to be added for refresh.
+
+        Returns:
+            None
+        """
+
         if is_model_instance(entity):
             self.entities_to_refresh.append(entity)
 
@@ -45,5 +55,15 @@ def is_model_instance(obj):
 
 @asynccontextmanager
 async def transaction(session: Optional[AsyncSession] = None):
+    """
+    A context manager that handles transactions using a SessionTransactionManager.
+
+    Args:
+        session: An optional AsyncSession object. Defaults to None.
+
+    Yields:
+        The transaction object.
+    """
+
     async with SessionTransactionManager(session=session) as txn:
         yield txn
